587b48955b43012b47632da35615346cb48e88c4
Leon Sell <sellleon@fim.uni-passau.de>
2019-01-05 12:46:57 +0100

    Add missing import

--- app/src/main/java/org/mate/exploration/genetic/HeuristicalChromosomeFactory.java
+++ app/src/main/java/org/mate/exploration/genetic/HeuristicalChromosomeFactory.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import static org.mate.ui.ActionType.BACK;
 import static org.mate.ui.ActionType.MENU;
