85332187b6199745edc6909b0f17e695d6ebfea9
Michael Auer <auermich@fim.uni-passau.de>
2019-05-08 12:58:20 +0200

    added selection function

---
+++ app/src/androidTest/java/org/mate/ExecuteMATERandomWalk.java
@@ -0,0 +1,18 @@
+package org.mate;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExecuteMATERandomWalk {
+
+        @Test
+        public void useAppContext() throws Exception {
+            MATE.log_acc("Starting Random Walk ....");
+
+            MATE mate = new MATE();
+            mate.testApp("RandomWalk");
+        }
+}
