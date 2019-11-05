f886e78b1761e5b8cb9366b56411ec1653a2d9be
Michael Auer <auermich@fim.uni-passau.de>
2019-11-05 12:21:35 +0100

    added retrieveFitnessValues function for branch distance

--- app/src/main/java/org/mate/exploration/genetic/fitness/BranchDistanceFitnessFunction.java
+++ app/src/main/java/org/mate/exploration/genetic/fitness/BranchDistanceFitnessFunction.java
@@ -1,5 +1,9 @@
 package org.mate.exploration.genetic.fitness;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.InstrumentationRegistry;
+
 import org.mate.MATE;
 import org.mate.exploration.genetic.chromosome.IChromosome;
 import org.mate.model.TestCase;
@@ -28,5 +32,26 @@ public class BranchDistanceFitnessFunction implements IFitnessFunction<TestCase>
         MATE.log("Branch Distance: " + fitness);
         cache.put(chromosome, fitness);
         return fitness;
+    }
+
+    /**
+     * Computes the fitness values for a given test case. As a side effect, an
+     * intent is sent to the injected broadcast receiver of the AUT, which
+     * stores the collected traces in the app-internal storage. Also updates
+     * the visited branches, in case branch coverage information is needed.
+     *
+     * @param chromosome The test case for which we want to evaluate its fitness values.
+     */
+    public static void retrieveFitnessValues(IChromosome<TestCase> chromosome) {
+
+        Intent intent = new Intent("STORE_TRACES");
+        Bundle bundle = new Bundle();
+        bundle.putString("packageName", MATE.packageName);
+        intent.putExtras(bundle);
+
+        InstrumentationRegistry.getTargetContext().sendBroadcast(intent);
+
+        double branchDistance = EnvironmentManager.getBranchDistance(chromosome);
+        MATE.log("Branch Distance: " + branchDistance);
     }
 }
