f31d31f87b66c4156f32326da889b0e8121ae807
Michael Auer <auermich@fim.uni-passau.de>
2019-12-05 14:39:17 +0100

    integrated intent based actions

--- app/src/main/java/org/mate/exploration/heuristical/HeuristicExploration.java
+++ app/src/main/java/org/mate/exploration/heuristical/HeuristicExploration.java
@@ -3,13 +3,23 @@ package org.mate.exploration.heuristical;
 import org.mate.MATE;
 import org.mate.Properties;
 import org.mate.exploration.genetic.chromosome_factory.HeuristicalChromosomeFactory;
+import org.mate.exploration.genetic.chromosome_factory.IChromosomeFactory;
+import org.mate.exploration.intent.IntentChromosomeFactory;
 
 public class HeuristicExploration {
-    private final HeuristicalChromosomeFactory heuristicChromosomeFactory;
+
+    // TODO: use here chromosome factory interface
+    private final IChromosomeFactory heuristicChromosomeFactory;
     private final boolean alwaysReset;
 
     public HeuristicExploration(int maxNumEvents) {
         this(Properties.STORE_COVERAGE(), false, maxNumEvents);
+    }
+
+    // TODO: we need some sort of combined factory that makes use of heuristic exploration with intent based actions
+    public HeuristicExploration(boolean storeCoverage, boolean alwaysReset, int maxNumEvents, float relativeIntentAmount) {
+        this.alwaysReset = alwaysReset;
+        heuristicChromosomeFactory = new IntentChromosomeFactory(storeCoverage, alwaysReset, maxNumEvents, relativeIntentAmount);
     }
 
     public HeuristicExploration(boolean storeCoverage, boolean alwaysReset, int maxNumEvents) {
---
+++ app/src/main/java/org/mate/interaction/intent/IntentBasedAction.java
@@ -0,0 +1,8 @@
+package org.mate.interaction.intent;
+
+import org.mate.ui.Action;
+
+public class IntentBasedAction extends Action {
+
+
+}
