c19f402699a8c166a57b4ba0934c0f9ee081d3dd
Michael Auer <auermich@fim.uni-passau.de>
2019-12-06 14:38:27 +0100

    added a representation for components and their intent filters

---
+++ app/src/main/java/org/mate/interaction/intent/ComponentDescription.java
@@ -0,0 +1,58 @@
+package org.mate.interaction.intent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class ComponentDescription {
+
+    private final String name;
+    private final ComponentType type;
+
+    // a component may define optionally intent filter tags
+    private Set<IntentFilterDescription> intentFilters = new HashSet<>();
+
+    ComponentDescription(String name, String type) {
+        this.name = name;
+        this.type = ComponentType.mapStringToComponent(type);
+    }
+
+    void addIntentFilter(IntentFilterDescription intentFilter) {
+        intentFilters.add(intentFilter);
+    }
+
+    private enum ComponentType {
+
+        ACTIVITY,
+        SERVICE,
+        BROADCAST_RECEIVER,
+        CONTENT_PROVIDER;
+
+        /**
+         * Maps the string representation to a component type if present.
+         *
+         * @param component The name of the component.
+         * @return Returns the component type matching the given component.
+         */
+        static ComponentType mapStringToComponent(String component) {
+            for (ComponentType type : ComponentType.values()) {
+                if (type.toString().equals(component)) {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("Component " + component + " cannot be mapped to a type!");
+        }
+
+        @Override
+        public String toString() {
+            // can also use super.toString() -> returns a string representation of the constants
+            switch (this) {
+                case ACTIVITY: return "activity";
+                case SERVICE: return "service";
+                case BROADCAST_RECEIVER: return "receiver";
+                case CONTENT_PROVIDER: return "provider";
+                default: throw new UnsupportedOperationException("Component type "
+                        + this + " not yet supported!");
+            }
+        }
+    }
+}
