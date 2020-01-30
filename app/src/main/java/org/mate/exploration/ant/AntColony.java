d37dc7d8dee51ac31886a92a6629049efbffd53d
reissi01 <REISSI01@gw.uni-passau.de>
2020-01-30 15:11:18 +0100

    Add ant logger

--- app/src/main/java/org/mate/exploration/ant/AntColony.java
+++ app/src/main/java/org/mate/exploration/ant/AntColony.java
@@ -1,13 +1,52 @@
 package org.mate.exploration.ant;
 
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.mate.MATE;
 import org.mate.Properties;
 import org.mate.exploration.genetic.fitness.LineCoveredPercentageFitnessFunction;
+import org.mate.interaction.UIAbstractionLayer;
+import org.mate.model.TestCase;
+import org.mate.ui.WidgetAction;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
 
 public class AntColony {
+    private final UIAbstractionLayer uiAbstractionLayer;
+    private final AntStatsLogger antStatsLogger;
+
+    public AntColony() {
+        uiAbstractionLayer = MATE.uiAbstractionLayer;
+        antStatsLogger = new AntStatsLogger();
+    }
+
     public void run() {
+
         //TODO start algorithm
         String targetLine = Properties.TARGET_LINE();
         LineCoveredPercentageFitnessFunction lineCoveredPercentageFitnessFunction
                 = new LineCoveredPercentageFitnessFunction(targetLine);
+
+        //Liste an momentan möglichen Aktionen
+        List<WidgetAction> executableActions = uiAbstractionLayer.getExecutableActions();
+
+        //Aktion ausführen
+        TestCase testCase = TestCase.newInitializedTestCase();
+        testCase.updateTestCase(executableActions.get(0), "0");
+
+        //Daten in Datei schreiben
+        antStatsLogger.write("asdf\n");
+        //... am Schluss zu machen
+        antStatsLogger.close();
+
     }
 }
---
+++ app/src/main/java/org/mate/exploration/ant/AntStatsLogger.java
@@ -0,0 +1,43 @@
+package org.mate.exploration.ant;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class AntStatsLogger {
+    private FileOutputStream fos;
+    private BufferedWriter writer;
+
+    public AntStatsLogger() {
+        try {
+            fos = InstrumentationRegistry.getTargetContext().openFileOutput("port", Context.MODE_PRIVATE);
+        } catch (FileNotFoundException e) {
+            //Fehlerbehandlung
+        }
+        writer = new BufferedWriter(new OutputStreamWriter(fos));
+    }
+
+    public void write(String log) {
+        try {
+            writer.write(log);
+            writer.flush();
+        } catch (IOException e) {
+            //Fehlerbehandlung
+        }
+    }
+
+    public void close() {
+        try {
+            writer.close();
+            fos.close();
+        } catch (IOException e) {
+            //Fehlerbehandlung
+            throw new IllegalStateException("Error while closing ant stats logger", e);
+        }
+    }
+}
