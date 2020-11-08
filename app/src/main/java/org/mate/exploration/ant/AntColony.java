f3e12b1b4dbb5fff5431e829f3725356d7150c7c
reissi01 <REISSI01@gw.uni-passau.de>
2020-11-08 11:08:51 +0100

    Set ACO parameters to new Condition 2 (ActionTypeInTransition = true, RankingDeposit = false)

--- app/src/main/java/org/mate/exploration/ant/AntColony.java
+++ app/src/main/java/org/mate/exploration/ant/AntColony.java
@@ -62,7 +62,7 @@ public class AntColony {
     *           deposit any pheromones
     *  False:   Only the ant with the best fitness value in a generation gets to deposit pheromones
     */
-    private static final boolean depositPheromonesWithRanking = true;
+    private static final boolean depositPheromonesWithRanking = false;
 
     public AntColony() {
         uiAbstractionLayer = MATE.uiAbstractionLayer;
