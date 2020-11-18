4e92995a965ebded2eaa0b8e15da0fa5e12f5c81
Leon Sell <sellleon@fim.uni-passau.de>
2020-11-18 14:00:45 +0100

    Fix StandardGE Android Test

--- app/src/androidTest/java/org/mate/ExecuteMATEStandardGE.java
+++ app/src/androidTest/java/org/mate/ExecuteMATEStandardGE.java
@@ -4,6 +4,27 @@ import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mate.exploration.genetic.algorithm.StandardGeneticAlgorithm;
+import org.mate.exploration.genetic.chromosome_factory.IntegerSequenceChromosomeFactory;
+import org.mate.exploration.genetic.core.IGeneticAlgorithm;
+import org.mate.exploration.genetic.crossover.IntegerSequencePointCrossOverFunction;
+import org.mate.exploration.genetic.fitness.BranchCoverageFitnessFunction;
+import org.mate.exploration.genetic.fitness.GenotypePhenotypeMappedFitnessFunction;
+import org.mate.exploration.genetic.fitness.IFitnessFunction;
+import org.mate.exploration.genetic.mutation.IntegerSequencePointMutationFunction;
+import org.mate.exploration.genetic.selection.FitnessProportionateSelectionFunction;
+import org.mate.exploration.genetic.termination.NeverTerminationCondition;
+import org.mate.exploration.genetic.util.ge.AndroidListBasedBiasedMapping;
+import org.mate.interaction.UIAbstractionLayer;
+import org.mate.model.TestCase;
+import org.mate.utils.Coverage;
+import org.mate.utils.TimeoutRun;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+import static org.mate.Properties.GE_TEST_CASE_ENDING_BIAS_PER_TEN_THOUSAND;
 
 @RunWith(AndroidJUnit4.class)
 public class ExecuteMATEStandardGE {
@@ -13,6 +34,26 @@ public class ExecuteMATEStandardGE {
         MATE.log_acc("Starting Standard GE Algorithm...");
 
         MATE mate = new MATE();
-        mate.testApp("StandardGE");
+
+        List<IFitnessFunction<List<Integer>>> fitnessFunctions = new ArrayList<>();
+        fitnessFunctions.add(new GenotypePhenotypeMappedFitnessFunction<>(
+                new AndroidListBasedBiasedMapping(GE_TEST_CASE_ENDING_BIAS_PER_TEN_THOUSAND()),
+                new BranchCoverageFitnessFunction<TestCase>()
+        ));
+
+        final IGeneticAlgorithm<List<Integer>> standardGE = new StandardGeneticAlgorithm<>(
+                new IntegerSequenceChromosomeFactory(Properties.GE_SEQUENCE_LENGTH()),
+                new FitnessProportionateSelectionFunction<List<Integer>>(),
+                new IntegerSequencePointCrossOverFunction(),
+                new IntegerSequencePointMutationFunction(),
+                fitnessFunctions,
+                new NeverTerminationCondition(),
+                Properties.POPULATION_SIZE(),
+                Properties.POPULATION_SIZE() * 2,
+                Properties.P_CROSSOVER(),
+                Properties.P_MUTATE()
+        );
+
+        mate.testApp(standardGE);
     }
 }
