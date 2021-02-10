efebf12b42c3d32856f643f713da3abc5077894e
Michael <auermich@fim.uni-passau.de>
2021-02-10 19:03:20 +0100

    added documentation + fixed code layout + minor refactoring

--- app/src/main/java/org/mate/state/executables/ActionsScreenState.java
+++ app/src/main/java/org/mate/state/executables/ActionsScreenState.java
@@ -8,6 +8,7 @@ import org.mate.state.IScreenState;
 import org.mate.state.ScreenStateType;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
@@ -24,7 +25,8 @@ public class ActionsScreenState extends AbstractScreenState {
     private List<WidgetAction> actions;
 
     /**
-     *
+     * TODO: What is the purpose of this?
+     * Tracks per activity (app screen) each widget and the number of ids.
      */
     private static Map<String, Map<String, List<Integer>>> idSizes = new Hashtable<>();
 
@@ -45,170 +47,189 @@ public class ActionsScreenState extends AbstractScreenState {
         this.appScreen = appScreen;
     }
 
-    private int getMaxAmountOfID(Map<String, List<Integer>> sameIDWidgets, String wid){
-         List<Integer> amounts = sameIDWidgets.get(wid);
-        if (amounts==null) {
+    /**
+     * Returns the maximal number of ...
+     *
+     * @param sameIDWidgets Stores a mapping of widget id to the amount the widget appeared.
+     * @param widgetId The widget id.
+     * @return Returns the maximal number of ... or zero if no entry for the widget id exists.
+     */
+    private int getMaxAmountOfID(Map<String, List<Integer>> sameIDWidgets, String widgetId) {
+
+        List<Integer> amounts = sameIDWidgets.get(widgetId);
+
+        if (amounts == null) {
             amounts = new ArrayList<>();
-            sameIDWidgets.put(wid,amounts);
+            sameIDWidgets.put(widgetId, amounts);
             return 0;
         }
 
-        if (amounts.size()==0)
+        if (amounts.size() == 0) {
             return 0;
-
-        int max = amounts.get(0);
-        int i=1;
-        while (i<amounts.size()){
-            if (max<amounts.get(i))
-                max = amounts.get(i);
-            i++;
+        } else {
+            return Collections.max(amounts);
         }
-        return max;
     }
 
     /**
+     * Extracts the list of applicable widget actions on the underlying screen.
      *
-     * @return
+     * @return Returns the list of widget actions.
      */
     @Override
     public List<WidgetAction> getActions() {
-        if (actions!=null)
+
+        if (actions != null)
             return actions;
 
-        Map<String,List<Integer>> sameIDWidgets = idSizes.get(activityName);
-        if (sameIDWidgets==null){
+        // TODO: what is the purpose of this?
+        Map<String, List<Integer>> sameIDWidgets = idSizes.get(activityName);
+
+        if (sameIDWidgets == null) {
             sameIDWidgets = new Hashtable<>();
-            idSizes.put(activityName,sameIDWidgets);
+            idSizes.put(activityName, sameIDWidgets);
         }
 
+        // collect the executable widget actions
         List<WidgetAction> executables = new ArrayList<>();
-        int editables = 0;
+
+        // track whether some editable widget was found
+        boolean foundEditable = false;
+
+        // track whether we have already added the action 'ENTER'
         boolean enterAdded = false;
-        Hashtable<String,Integer> idAmount = new Hashtable<String,Integer>();
+
+        // track whether the current widget is selected to have a widget action
         boolean selected;
 
-        // TODO: move the following code in an own method 'hostsAction()' of the class Widget
-        for (Widget widget: widgets){
+        // a counter how often the same widget id (key) appears
+        Map<String, Integer> idAmount = new Hashtable<>();
+
+        for (Widget widget: widgets) {
+
             selected = false;
 
-            if (widget.getClazz().contains("Button"))
-                selected=true;
-
-            if (widget.isClickable() || widget.isLongClickable() || widget.isScrollable() || widget.isEditable() ){
-                selected=true;
+            if (widget.getClazz().contains("Button") || widget.isClickable()
+                    || widget.isLongClickable()
+                    || widget.isScrollable()
+                    || widget.isEditable()) {
+                selected = true;
             }
 
-            if (widget.directSonOf("ListView")||widget.directSonOf("GridView")) {
+            if (widget.directSonOf("ListView") || widget.directSonOf("GridView")) {
                 if (widget.getParent().isClickable())
                     selected = true;
-
             }
 
-//            if (widget.getClazz().contains("ImageView"))
-//                selected=true;
-
-            if (widget.getClazz().equals("android.view.View")&&(!widget.getContentDesc().equals("")||!widget.getText().equals(""))){
-                selected=true;
+            if (widget.getClazz().equals("android.view.View")
+                    && (!widget.getContentDesc().isEmpty() || !widget.getText().isEmpty())) {
+                selected = true;
             }
 
-            if (widget.getClazz().contains("Spinner")){
-                selected=true;
+            if (widget.getClazz().contains("Spinner")) {
+                selected = true;
             }
 
-            //for skype test
-            if (widget.getClazz().contains("ViewGroup")){
-                if (!widget.getContentDesc().equals(""))
-                    selected=true;
+            // for skype test
+            if (widget.getClazz().contains("ViewGroup")) {
+                if (!widget.getContentDesc().isEmpty())
+                    selected = true;
             }
 
+            /*
+             * TODO: Review this. It seems like to ignore redundant actions for the same widget,
+             *  or more specifically for widgets with the same id.
+             */
             Integer amount = idAmount.get(widget.getId());
-            if (amount==null){
-                idAmount.put(widget.getId(),1);
+
+            if (amount == null) {
+                idAmount.put(widget.getId(), 1);
                 amount = idAmount.get(widget.getId());
-            }
-            else{
-                idAmount.put(widget.getId(),++amount);
+            } else {
+                idAmount.put(widget.getId(), ++amount);
                 amount = idAmount.get(widget.getId());
             }
 
-            if (amount>getMaxAmountOfID(sameIDWidgets,widget.getId()) && sameIDWidgets.get(widget.getId()).size()==2){
-                selected=false;
+            if (amount > getMaxAmountOfID(sameIDWidgets, widget.getId())
+                    && sameIDWidgets.get(widget.getId()).size() == 2) {
+                selected = false;
             }
 
-            if (selected){
+            if (selected) {
                 if (widget.getClazz().equals("android.widget.GridView"))
-                    selected=false;
+                    selected = false;
             }
 
-            if (selected){
-                if (widget.getClazz().equals("android.view.View")){
-                    if (!widget.isClickable()&&!widget.isLongClickable()&&!widget.isScrollable()&&widget.getText().equals("")&&widget.getContentDesc().equals("")){
-                        selected=false;
+            if (selected) {
+                if (widget.getClazz().equals("android.view.View")) {
+                    if (!widget.isClickable() && !widget.isLongClickable() && !widget.isScrollable()
+                            && widget.getText().isEmpty() && widget.getContentDesc().isEmpty()) {
+                        selected = false;
                     }
                 }
             }
 
-            if (selected){
+            if (selected) {
                 if (widget.getClazz().contains("ListView"))
-                    selected=false;
+                    selected = false;
             }
 
-            if (selected){
+            if (selected) {
                 if (widget.getClazz().contains("ScrollView"))
-                    selected=false;
+                    selected = false;
             }
 
-            if (selected){
+            if (selected) {
                 if (widget.isSonOf("android.webkit.WebView"))
-                    selected=false;
+                    selected = false;
             }
 
             if (!widget.isEnabled())
-                selected=false;
+                selected = false;
 
-            if (selected){
+            if (selected) {
+
+                // derive the appropriate widget action
                 WidgetAction event;
-                if (!widget.isEditable()){
+
+                if (!widget.isEditable()) {
                     event = new WidgetAction(widget, ActionType.CLICK);
                     executables.add(event);
                     widget.setClickable(true);
                 }
 
-                if (widget.isEditable() || widget.getClazz().contains("Edit")){
-                    event = new WidgetAction(widget,ActionType.TYPE_TEXT);
-                    executables.add(0,event);
-                    editables++;
+                if (widget.isEditable() || widget.getClazz().contains("Edit")) {
+                    event = new WidgetAction(widget, ActionType.TYPE_TEXT);
+                    executables.add(0, event);
+                    foundEditable = true;
                 }
 
-
-                if (widget.isLongClickable()&&!widget.isEditable()){
+                if (widget.isLongClickable() && !widget.isEditable()) {
                     event = new WidgetAction(widget, ActionType.LONG_CLICK);
                     executables.add(event);
                     widget.setLongClickable(true);
-                }
-                else
-                {
-                    if ((widget.isSonOfLongClickable()) && (!widget.isEditable()&& !widget.getClazz().contains("TextView"))){
+                } else {
+                    if ((widget.isSonOfLongClickable()) && (!widget.isEditable()
+                            && !widget.getClazz().contains("TextView"))) {
                         event = new WidgetAction(widget, ActionType.LONG_CLICK);
                         executables.add(event);
                         widget.setLongClickable(true);
                     }
                 }
 
+                if (widget.isScrollable()) {
 
-                if (widget.isScrollable()){
-
-                    if (!widget.getClazz().contains("Spinner")&&!widget.isSonOf("Spinner")){
-                        event = new WidgetAction(widget,ActionType.SWIPE_LEFT);
+                    if (!widget.getClazz().contains("Spinner") && !widget.isSonOf("Spinner")) {
+                        event = new WidgetAction(widget, ActionType.SWIPE_LEFT);
                         executables.add(event);
 
-                        event = new WidgetAction(widget,ActionType.SWIPE_RIGHT);
+                        event = new WidgetAction(widget, ActionType.SWIPE_RIGHT);
                         executables.add(event);
 
-                        event = new WidgetAction(widget,ActionType.SWIPE_UP);
+                        event = new WidgetAction(widget, ActionType.SWIPE_UP);
                         executables.add(event);
 
-                        event = new WidgetAction(widget,ActionType.SWIPE_DOWN);
+                        event = new WidgetAction(widget, ActionType.SWIPE_DOWN);
                         executables.add(event);
 
                         widget.setScrollable(true);
@@ -216,49 +237,63 @@ public class ActionsScreenState extends AbstractScreenState {
                 }
             }
 
-            if (editables>0 && !enterAdded){
-                executables.add(editables,new WidgetAction(ActionType.ENTER));
-                enterAdded=true;
+            // if we found some editable widget, we should add the action 'ENTER'
+            if (foundEditable && !enterAdded) {
+                executables.add(new WidgetAction(ActionType.ENTER));
+                enterAdded = true;
             }
         }
 
-        //update number of ids
-        for (String id: idAmount.keySet()){
+        // TODO: review this
+        // update number of ids
+        for (String id: idAmount.keySet()) {
             List<Integer> amounts = sameIDWidgets.get(id);
-            if (amounts.size()<2){
-                boolean sameAmount=false;
-                for (int i=0; i<amounts.size(); i++)
-                    if (amounts.get(i)==idAmount.get(id))
-                        sameAmount=true;
+            if (amounts.size() < 2) {
+                boolean sameAmount = false;
+                for (int i = 0; i < amounts.size(); i++)
+                    if (amounts.get(i) == idAmount.get(id))
+                        sameAmount = true;
                 if (!sameAmount)
                     amounts.add(idAmount.get(id));
             }
         }
 
-        if (executables.size()==0)
-            executables.add(new WidgetAction(ActionType.BACK));
-
-        if (appScreen.isHasToScrollDown()||appScreen.isHasToScrollUp()){
+        if (appScreen.isHasToScrollDown() || appScreen.isHasToScrollUp()) {
             executables.add(new WidgetAction(ActionType.SWIPE_DOWN));
             executables.add(new WidgetAction(ActionType.SWIPE_UP));
         }
 
-        if (appScreen.isHasToScrollLeft()||appScreen.isHasToScrollRight()){
+        if (appScreen.isHasToScrollLeft() || appScreen.isHasToScrollRight()) {
             executables.add(new WidgetAction(ActionType.SWIPE_LEFT));
             executables.add(new WidgetAction(ActionType.SWIPE_RIGHT));
         }
 
+        // TODO: maybe we should only allow to press 'BACK', since we can't authenticate
         if (activityName.contains("GoogleOAuthActivity"))
             executables = new ArrayList<>();
-        executables.add(new WidgetAction(ActionType.BACK));
-        executables.add(new WidgetAction(ActionType.MENU));
 
         // those actions should be always applicable independent of the widget
+        executables.addAll(getWidgetIndependentActions());
+
+        actions = executables;
+        return executables;
+    }
+
+    /**
+     * Returns the list of actions are applicable independent on any widgets.
+     *
+     * @return Returns the list of actions applicable on any screen.
+     */
+    private List<WidgetAction> getWidgetIndependentActions() {
+
+        List<WidgetAction> executables = new ArrayList<>();
+        executables.add(new WidgetAction(ActionType.BACK));
+        executables.add(new WidgetAction(ActionType.MENU));
         executables.add(new WidgetAction(ActionType.TOGGLE_ROTATION));
-        //executables.add(new WidgetAction(ActionType.HOME));
+        // executables.add(new WidgetAction(ActionType.HOME));
         executables.add(new WidgetAction(ActionType.SEARCH));
-        //executables.add(new WidgetAction(ActionType.QUICK_SETTINGS));
-        //executables.add(new WidgetAction(ActionType.NOTIFICATIONS));
+        // executables.add(new WidgetAction(ActionType.QUICK_SETTINGS));
+        // executables.add(new WidgetAction(ActionType.NOTIFICATIONS));
         executables.add(new WidgetAction(ActionType.SLEEP));
         executables.add(new WidgetAction(ActionType.WAKE_UP));
         executables.add(new WidgetAction(ActionType.DELETE));
@@ -267,8 +302,6 @@ public class ActionsScreenState extends AbstractScreenState {
         executables.add(new WidgetAction(ActionType.DPAP_UP));
         executables.add(new WidgetAction(ActionType.DPAD_LEFT));
         executables.add(new WidgetAction(ActionType.DPAD_RIGHT));
-
-        actions = executables;
         return executables;
     }
 
