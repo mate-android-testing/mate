938da8536788e47cc468e772b8919f0b6adab740
Michael Pusl <pusl03@ads.uni-passau.de>
2021-09-09 17:26:32 +0200

    Content: QBEState memorize actions and feature vector.
    Signed-off-by: Michael Pusl <pusl03@ads.uni-passau.de>

--- app/src/main/java/org/mate/exploration/qlearning/qbe/interfaces/implementations/QBEState.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/interfaces/implementations/QBEState.java
@@ -5,8 +5,11 @@ import android.support.annotation.RequiresApi;
 
 import org.mate.exploration.qlearning.qbe.interfaces.State;
 import org.mate.exploration.qlearning.qbe.interfaces.StateSkeleton;
+import org.mate.interaction.action.ui.Widget;
 import org.mate.state.IScreenState;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
@@ -16,18 +19,33 @@ import java.util.stream.Collectors;
 @RequiresApi(api = Build.VERSION_CODES.N)
 public final class QBEState extends StateSkeleton<QBEAction> implements State<QBEAction> {
 
-    private final IScreenState screenState;
+    private final Set<QBEAction> actions;
+    private final Map<String, Integer> featureMap;
 
     public QBEState(final IScreenState screenState) {
-        this.screenState = Objects.requireNonNull(screenState);
+        Objects.requireNonNull(screenState);
+        featureMap = computeFeatureMap(screenState.getWidgets());
+        actions = screenState.getActions().stream().map(QBEAction::new).collect(Collectors.toSet());
     }
 
     public QBEState(final QBEState state) {
-        this(state.screenState);
+        // Note: Shallow copies are ok, because both fields are read-only.
+        Objects.requireNonNull(state);
+        actions = state.actions;
+        featureMap = state.featureMap;
+    }
+
+    private static Map<String, Integer> computeFeatureMap(final List<Widget> widgets) {
+        final Map<String, Long> counts = widgets.stream()
+                .map(widget -> widget.getClazz() + "@" + widget.getDepth())
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+        return counts.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().intValue()));
     }
 
     public Set<QBEAction> getActions() {
-        return screenState.getActions().stream().map(QBEAction::new).collect(Collectors.toSet());
+        return Collections.unmodifiableSet(actions);
     }
 
     @Override
@@ -38,12 +56,6 @@ public final class QBEState extends StateSkeleton<QBEAction> implements State<QB
 
     @Override
     protected Map<String, Integer> getFeatureMap() {
-        final Map<String, Long> counts = screenState.getWidgets()
-                .stream()
-                .map(widget -> widget.getClazz() + "@" + widget.getDepth())
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-        return counts.entrySet()
-                .stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, e -> e.getValue().intValue()));
+        return Collections.unmodifiableMap(featureMap);
     }
 }
