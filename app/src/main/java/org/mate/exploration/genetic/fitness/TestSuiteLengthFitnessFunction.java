876727a48eefc76036cc46f552834bced127e89c
Severin Primbs <primbs@fim.uni-passau.de>
2021-09-16 11:25:58 +0200

    changed to a maximizing fitness function, renamed to TestSuiteLengthFitnessFunction.

--- app/src/main/java/org/mate/exploration/genetic/fitness/TestSuiteLengthFitnessFunction.java
+++ app/src/main/java/org/mate/exploration/genetic/fitness/TestSuiteLengthFitnessFunction.java
@@ -5,7 +5,7 @@ import org.mate.exploration.genetic.fitness.IFitnessFunction;
 import org.mate.model.TestCase;
 import org.mate.model.TestSuite;
 
-public class TestLengthFitnessFunction implements IFitnessFunction<TestSuite> {
+public class TestSuiteLengthFitnessFunction implements IFitnessFunction<TestSuite> {
 
     @Override
     public double getFitness(IChromosome<TestSuite> chromosome) {
@@ -16,9 +16,15 @@ public class TestLengthFitnessFunction implements IFitnessFunction<TestSuite> {
         return 1.0 / lengthSum;
     }
 
+    /*
+      The intention of this fitness function is to prefer chromosomes with a lower test case
+      length. Considering the way to compute the fitness value it's a maximizing function.
+      Assume you have a test suite with 4 and 8 test cases, then the respective fitness values
+      are 1/4 and 1/8 according to getFitness(). Obviously, 1/4 is greater than 1/8, thus better.
+     */
     @Override
     public boolean isMaximizing() {
-        return false;
+        return true;
     }
 
     @Override
