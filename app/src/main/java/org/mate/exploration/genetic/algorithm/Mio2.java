d03edda8e7b08c39cebcbdd10eed834901656024
Michael <auermich@fim.uni-passau.de>
2021-09-29 22:42:12 +0200

    added mutation rate + documentation

--- app/src/main/java/org/mate/exploration/genetic/algorithm/Mio2.java
+++ app/src/main/java/org/mate/exploration/genetic/algorithm/Mio2.java
@@ -23,6 +23,13 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * An implementation of the Many Independent Objective (MIO) Algorithm according to the paper
+ * "Many Independent Objective (MIO) Algorithm for Test Suite Generation", see
+ * https://arxiv.org/abs/1901.01541 for more details.
+ *
+ * @param <T> Refers to the type of chromosome. Traditionally, MIO uses {@link TestCase}s as chromosomes.
+ */
 public class Mio2<T> extends GeneticAlgorithm<T> {
 
     private Map<IFitnessFunction<T>, List<ChromosomeFitnessTuple>> archive;
@@ -42,6 +49,9 @@ public class Mio2<T> extends GeneticAlgorithm<T> {
     // keep the initial values of n and PR
     private final int populationSizeStart;
     private final double pSampleRandomStart;
+
+    // how many mutations should be performed until the next sampling, see variable m
+    private int mutations;
 
     // see variable ck
     private final Map<IFitnessFunction<T>, Integer> samplingCounters;
@@ -82,6 +92,7 @@ public class Mio2<T> extends GeneticAlgorithm<T> {
         this.pSampleRandomStart = pSampleRandom;
         this.populationSizeStart = populationSize;
         this.samplingCounters = new HashMap<>();
+        this.mutations = 1;
 
         for (IFitnessFunction<T> fitnessFunction : fitnessFunctions) {
 
@@ -93,61 +104,122 @@ public class Mio2<T> extends GeneticAlgorithm<T> {
         }
     }
 
+    /**
+     * MIO initially samples a random chromosome, evaluates its fitness and updates the
+     * archive accordingly.
+     */
     @Override
     public void createInitialPopulation() {
 
         this.startTime = System.currentTimeMillis();
 
-        MATE.log_acc("Generating initial population!");
+        MATE.log_acc("Generating population # " + (currentGenerationNumber + 1) + "!");
         IChromosome<T> chromosome = chromosomeFactory.createChromosome();
+        population.add(chromosome);
 
         for (IFitnessFunction<T> fitnessFunction : this.fitnessFunctions) {
             double fitness = fitnessFunction.getNormalizedFitness(chromosome);
-            archive.get(fitnessFunction).add(new ChromosomeFitnessTuple(chromosome, fitness));
+            addToArchive(fitnessFunction, new ChromosomeFitnessTuple(chromosome, fitness));
         }
 
         logCurrentFitness();
         currentGenerationNumber++;
     }
 
+    /**
+     * Section 3.1: Core Algorithm
+     *
+     * From the second iteration on, MIO either samples a random chromosome with probability P_R
+     * or samples a chromosome from the archive with probability (1 - P_R) and mutates it. Then,
+     * the fitness of the chromosome is evaluated (h_k) and the archive is updated.
+     *
+     * For each target k, the chromosome is saved in the population T_k with |T_k| < n, if either:
+     *
+     *  - If h_k = 0 (i.e. target unreachable), then the chromosome is not added to any population
+     *    regardless of the following conditions.
+     *  - If h_k = 1 (i.e. target covered), the chromosome is added to the population and this
+     *    population is shrunk to one chromosome and will never expand again. If the population
+     *    already contains a chromosome that covers the target, then the new chromosome will only
+     *    replace the present chromosome if it is shorter or in case it has the same size, only if
+     *    the sum of fitness values over every target other than k is better.
+     *  - If the population is not full, i.e. |T_k| < n, then the chromosome is added.
+     *  - If the population is full, i.e. |T_k| = n, the chromosome might replace the worst
+     *    chromosome in the population, but only if it is not worse in terms of fitness or in case
+     *    of equal fitness, only if it is shorter than the worst chromosome.
+     *
+     * When MIO needs to sample a chromosome from the archive, it picks the chromosome based on
+     * the following procedure:
+     *
+     *  1. It chooses the target k with the lowest sampling counter c_k, see section 3.3, where
+     *    T_k is not empty and k is not covered. If all non-empty populations are covered, then
+     *    k is chosen randomly among them.
+     *  2. It chooses a chromosome from T_k randomly.
+     *
+     *
+     * Section 3.2: Exploration/Exploitation Control
+     *
+     * MIO makes use of a focused search once a certain amount of the search budget is exhausted.
+     * The percentage F defines the time after which the focused search should start. This means
+     * that the values of the parameters P_R and n are dynamically adapted: initially they are set
+     * to some user-defined value and with the passing of time they decrease linearly. Once the
+     * focused search starts P_R is set to 0 and n is set to 1. For example, if P_R = 0.5 and
+     * F = 0.5 (i.e., the focused search starts after 50% of the search budget is used), then after
+     * 30% of the search, the value P_R is decreased from 0.5 to 0.2.
+     *
+     * MIO also introduces another parameter m that controls how many mutants should be generated
+     * from the same chromosome. This parameter also varies over time, e.g. it starts from 1 and
+     * increases up to 10 when the focused search begins.
+     *
+     *
+     * Section 3.3: Feedback-Directed Sampling
+     *
+     * Every target will be assigned a sampling counter c_k. Initially this counter is 0 for every
+     * target. When a chromosome is sampled from the population T_k, c_k is increased by one. If
+     * a chromosome is added to T_k or replaces a chromosome in it, c_k is reset to 0. When we
+     * sample from the archive, we choose the k with the lowest counter c_k from a population T_k
+     * that is not empty nor covered yet. If all populations T_k are covered, we choose k
+     * randomly from those covered populations.
+     */
     @Override
     public void evolve() {
 
-        MATE.log_acc("Generating population #" + (++currentGenerationNumber));
-
-        IChromosome<T> chromosome;
+        MATE.log_acc("Generating population #" + (currentGenerationNumber + 1));
+        population.clear();
 
         if (Randomness.getRnd().nextDouble() < pSampleRandom) {
             // sample random
-            chromosome = chromosomeFactory.createChromosome();
+            population.add(chromosomeFactory.createChromosome());
         } else {
-
-            // TODO: Allow up to m mutations of chromosome when we run focused search.
-
             // sample from archive, pick target with lowest sampling counter
             IFitnessFunction<T> target = getBestTarget();
 
             // take a random element from the chosen target population
             ChromosomeFitnessTuple tuple = Randomness.randomElement(archive.get(target));
-            chromosome = tuple.chromosome;
+            IChromosome<T> chromosome = tuple.chromosome;
 
             // increase sampling counter, see section 3.3
             Integer value = samplingCounters.get(target);
             samplingCounters.put(target, value + 1);
 
-            List<IChromosome<T>> mutated = mutationFunction.mutate(chromosome);
-            chromosome = mutated.get(0);
+            for (int i = 0; i < mutations; i++) {
+                List<IChromosome<T>> mutants = mutationFunction.mutate(chromosome);
+                population.add(mutants.get(0));
+            }
         }
 
-        for (IFitnessFunction<T> fitnessFunction : this.fitnessFunctions) {
-            double fitness = fitnessFunction.getNormalizedFitness(chromosome);
-            ChromosomeFitnessTuple tuple = new ChromosomeFitnessTuple(chromosome, fitness);
+        for (IChromosome<T> chromosome : population) {
+            for (IFitnessFunction<T> fitnessFunction : this.fitnessFunctions) {
 
-            // the new sampled chromosome might be added to one or more sub populations in the archive
-            modifyArchive(fitnessFunction, tuple);
+                // evaluate fitness and update archive
+                double fitness = fitnessFunction.getNormalizedFitness(chromosome);
+                ChromosomeFitnessTuple tuple = new ChromosomeFitnessTuple(chromosome, fitness);
+                updateArchive(fitnessFunction, tuple);
+            }
         }
 
         logCurrentFitness();
+        currentGenerationNumber++;
+
         updateParameters();
 
         // TODO: Remove! This just tries to keep the cache size reasonable by dropping unused chromosomes.
@@ -160,13 +232,8 @@ public class Mio2<T> extends GeneticAlgorithm<T> {
         FitnessUtils.cleanCache(activeChromosomes);
     }
 
-    @Override
-    protected void logCurrentFitness() {
-        // TODO: log fitness of last accessed chromosomes
-    }
-
     // section 3.1
-    private void modifyArchive(IFitnessFunction<T> fitnessFunction, ChromosomeFitnessTuple tuple) {
+    private void updateArchive(IFitnessFunction<T> fitnessFunction, ChromosomeFitnessTuple tuple) {
 
         if (isTargetNotReachable(fitnessFunction, tuple)) {
             // h = 0, the chromosome is not added to the archive at all
@@ -400,6 +467,8 @@ public class Mio2<T> extends GeneticAlgorithm<T> {
             startedFocusedSearch = true;
             pSampleRandom = 0.0;
             populationSize = 1;
+            // TODO: the mutation rate also linearly increases over time
+            mutations = 10;
         } else {
             double expiredTimePercent = expiredTime / (Registry.getTimeout() / 100);
             double decreasePercent = expiredTimePercent / (focusedSearchStart * 100);
