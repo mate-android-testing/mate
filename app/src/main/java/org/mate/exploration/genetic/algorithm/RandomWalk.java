69720864ab15772a918d630ef53e760d85011b1e
Michael <auermich@fim.uni-passau.de>
2021-10-22 13:21:17 +0200

    refactored random walk implementation

--- app/src/main/java/org/mate/exploration/genetic/algorithm/RandomWalk.java
+++ app/src/main/java/org/mate/exploration/genetic/algorithm/RandomWalk.java
@@ -1,35 +1,73 @@
 package org.mate.exploration.genetic.algorithm;
 
+import org.mate.MATE;
 import org.mate.exploration.genetic.chromosome.IChromosome;
 import org.mate.exploration.genetic.chromosome_factory.IChromosomeFactory;
 import org.mate.exploration.genetic.core.GeneticAlgorithm;
 import org.mate.exploration.genetic.fitness.IFitnessFunction;
 import org.mate.exploration.genetic.mutation.IMutationFunction;
-import org.mate.exploration.genetic.selection.IdSelectionFunction;
 import org.mate.exploration.genetic.termination.ITerminationCondition;
 
-import java.util.Arrays;
 import java.util.List;
 
+/**
+ * Provides a generic implementation of a random walk.
+ *
+ * @param <T> The type of the chromosome.
+ */
 public class RandomWalk<T> extends GeneticAlgorithm<T> {
 
     /**
-     * Initializing the genetic algorithm with all necessary attributes
+     * Initializes the random walk with all necessary attributes.
      *
-     * @param chromosomeFactory    see {@link IChromosomeFactory}
-     * @param mutationFunction     see {@link IMutationFunction}
-     * @param fitnessFunctions see {@link IFitnessFunction}
-     * @param terminationCondition see {@link ITerminationCondition}
+     * @param chromosomeFactory The used chromosome factory.
+     * @param mutationFunction The used mutation function.
+     * @param fitnessFunctions The list of fitness functions.
+     * @param terminationCondition The used termination condition.
      */
-    public RandomWalk(IChromosomeFactory<T> chromosomeFactory, IMutationFunction<T> mutationFunction, List<IFitnessFunction<T>> fitnessFunctions, ITerminationCondition terminationCondition) {
-        super(chromosomeFactory, new IdSelectionFunction<T>(), null, mutationFunction, fitnessFunctions, terminationCondition, 1, 2, 0, 1);
+    public RandomWalk(IChromosomeFactory<T> chromosomeFactory, IMutationFunction<T> mutationFunction,
+                      List<IFitnessFunction<T>> fitnessFunctions, ITerminationCondition terminationCondition) {
+        super(
+                chromosomeFactory,
+                null,
+                null,
+                mutationFunction,
+                fitnessFunctions,
+                terminationCondition,
+                1,
+                1,
+                0,
+                1);
     }
 
     /**
-     * Always return the new chromosome (offspring)
-     * @return new chromosome (offspring) only
+     * A random walk initially samples a random chromosome that is iteratively mutated.
      */
-    public List<IChromosome<T>> getGenerationSurvivors() {
-        return Arrays.asList(population.get(population.size() - 1));
+    @Override
+    public void createInitialPopulation() {
+        MATE.log_acc("Creating generation #" + (currentGenerationNumber + 1));
+        IChromosome<T> chromosome = chromosomeFactory.createChromosome();
+        population.add(chromosome);
+        logCurrentFitness();
+        currentGenerationNumber++;
+    }
+
+    /**
+     * Mutates the chromosome from the previous generation.
+     */
+    @Override
+    public void evolve() {
+
+        MATE.log_acc("Creating generation #" + (currentGenerationNumber + 1));
+
+        IChromosome<T> chromosome = population.get(0);
+        IChromosome<T> mutant = mutationFunction.mutate(chromosome);
+
+        // keep solely the mutant (offspring)
+        population.clear();
+        population.add(mutant);
+
+        logCurrentFitness();
+        currentGenerationNumber++;
     }
 }
