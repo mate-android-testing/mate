e1542fa3f89d17701cf98c7a938023a32b6cf4c6
Michael <auermich@fim.uni-passau.de>
2021-11-08 11:15:45 +0100

    make NSGA-II configurable via properties

--- app/src/androidTest/java/org/mate/ExecuteMATENSGAII.java
+++ app/src/androidTest/java/org/mate/ExecuteMATENSGAII.java
@@ -7,13 +7,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mate.exploration.genetic.algorithm.Algorithm;
 import org.mate.exploration.genetic.builder.GeneticAlgorithmBuilder;
-import org.mate.exploration.genetic.chromosome_factory.ChromosomeFactory;
 import org.mate.exploration.genetic.core.IGeneticAlgorithm;
-import org.mate.exploration.genetic.crossover.CrossOverFunction;
 import org.mate.exploration.genetic.fitness.FitnessFunction;
-import org.mate.exploration.genetic.mutation.MutationFunction;
 import org.mate.exploration.genetic.selection.SelectionFunction;
-import org.mate.exploration.genetic.termination.TerminationCondition;
 import org.mate.model.TestCase;
 
 @RunWith(AndroidJUnit4.class)
@@ -30,13 +26,18 @@ public class ExecuteMATENSGAII {
 
         IGeneticAlgorithm<TestCase> nsga = new GeneticAlgorithmBuilder()
                 .withAlgorithm(Algorithm.NSGAII)
-                .withChromosomeFactory(ChromosomeFactory.ANDROID_RANDOM_CHROMOSOME_FACTORY)
+                .withChromosomeFactory(Properties.CHROMOSOME_FACTORY())
                 .withSelectionFunction(SelectionFunction.CROWDED_TOURNAMENT_SELECTION)
-                .withCrossoverFunction(CrossOverFunction.TEST_CASE_MERGE_CROSS_OVER)
-                .withMutationFunction(MutationFunction.TEST_CASE_CUT_POINT_MUTATION)
+                .withCrossoverFunction(Properties.CROSSOVER_FUNCTION())
+                .withMutationFunction(Properties.MUTATION_FUNCTION())
                 .withFitnessFunction(FitnessFunction.NUMBER_OF_ACTIVITIES)
                 .withFitnessFunction(FitnessFunction.NUMBER_OF_STATES)
-                .withTerminationCondition(TerminationCondition.ITERATION_TERMINATION)
+                .withPopulationSize(Properties.POPULATION_SIZE())
+                .withBigPopulationSize(Properties.BIG_POPULATION_SIZE())
+                .withMaxNumEvents(Properties.MAX_NUMBER_EVENTS())
+                .withPMutate(Properties.P_MUTATE())
+                .withPCrossover(Properties.P_CROSSOVER())
+                .withTerminationCondition(Properties.TERMINATION_CONDITION())
                 .build();
 
         mate.testApp(nsga);
