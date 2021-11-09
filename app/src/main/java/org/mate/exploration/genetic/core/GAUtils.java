4964f37aa6cf7f1fc6b074c3d781d402106e01c8
Michael <auermich@fim.uni-passau.de>
2021-11-09 11:30:17 +0100

    minor refactoring - move domination comparator to dedicated class

--- app/src/main/java/org/mate/exploration/genetic/core/GAUtils.java
+++ app/src/main/java/org/mate/exploration/genetic/core/GAUtils.java
@@ -2,6 +2,7 @@ package org.mate.exploration.genetic.core;
 
 import org.mate.MATE;
 import org.mate.exploration.genetic.chromosome.IChromosome;
+import org.mate.exploration.genetic.comparator.DominationComparator;
 import org.mate.exploration.genetic.fitness.IFitnessFunction;
 import org.mate.model.TestCase;
 import org.mate.model.TestSuite;
@@ -159,7 +160,7 @@ public class GAUtils {
         Map<Integer, List<IChromosome<T>>> paretoFronts = new HashMap<>();
 
         // a comparator for two chromosomes based on domination relation
-        Comparator<IChromosome<T>> comparator = dominationComparator(fitnessFunctions);
+        Comparator<IChromosome<T>> comparator = new DominationComparator<>(fitnessFunctions);
 
         // maintains for each chromosome the set of dominated chromosomes
         Map<IChromosome<T>, Set<IChromosome<T>>> dominationSets = new HashMap<>();
@@ -245,84 +246,6 @@ public class GAUtils {
 
         MATE.log_acc("Number of pareto fronts: " + paretoFronts.size());
         return paretoFronts;
-    }
-
-    /**
-     * Provides a comparator that compares two chromosomes based on the domination relation.
-     *
-     * @param fitnessFunctions The list of fitness functions.
-     * @return Returns a comparator for the domination relation.
-     */
-    private static <T> Comparator<IChromosome<T>> dominationComparator(
-            final List<IFitnessFunction<T>> fitnessFunctions) {
-
-        return new Comparator<IChromosome<T>>() {
-
-            /**
-             *  Performs a comparison of two chromosomes based on the domination relation.
-             *
-             * @param o1 The first chromosome.
-             * @param o2 The second chromosome.
-             * @return Returns a value of {@code -1} if o2 dominates o1 or a value of {@code 1}
-             *          if o1 dominates o2. If no chromosome dominates each other, a value of
-             *          {@code 0} is returned.
-             */
-            @Override
-            public int compare(IChromosome<T> o1, IChromosome<T> o2) {
-
-                /*
-                 * Whether o1 is in at least one target better/worse than o2.
-                 */
-                boolean isBetterInOneTarget = false;
-                boolean isWorseInOneTarget = false;
-
-                for (IFitnessFunction<T> fitnessFunction : fitnessFunctions) {
-
-                    boolean isMaximising = fitnessFunction.isMaximizing();
-
-                    double fitness1 = fitnessFunction.getNormalizedFitness(o1);
-                    double fitness2 = fitnessFunction.getNormalizedFitness(o2);
-                    int cmp = Double.compare(fitness1, fitness2);
-
-                    if (cmp == 0) {
-                        // compare on next target
-                        continue;
-                    } else {
-
-                        if (!isMaximising) {
-                            // flip comparison for minimising fitness function
-                            cmp = cmp * -1;
-                        }
-
-                        if (cmp < 0) {
-                            // o2 dominates o1
-                            isWorseInOneTarget = true;
-                        } else {
-                            // o1 dominates o2
-                            isBetterInOneTarget = true;
-                        }
-
-                        if (isBetterInOneTarget && isWorseInOneTarget) {
-                            // no domination, hence same pareto front
-                            return 0;
-                        }
-                    }
-                }
-
-                if (!isWorseInOneTarget && !isBetterInOneTarget) {
-                    // equal in every target, hence no domination
-                    return 0;
-                } else if (isBetterInOneTarget && !isWorseInOneTarget) {
-                    // at least better in one target, hence o1 dominates o2
-                    return 1;
-                } else if (isWorseInOneTarget && !isBetterInOneTarget) {
-                    // at least worse in one target, hence o2 dominates o1
-                    return -1;
-                } else {
-                    throw new IllegalStateException("Should never happen!");
-                }
-            }
-        };
     }
 
     /**
