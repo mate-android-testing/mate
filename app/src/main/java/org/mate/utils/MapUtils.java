a3bc1fd007d588b4c02f2a25d1e23d14cbce3e74
Michael <auermich@fim.uni-passau.de>
2021-11-19 13:12:11 +0100

    apply suggested fix

--- app/src/main/java/org/mate/utils/MapUtils.java
+++ app/src/main/java/org/mate/utils/MapUtils.java
@@ -2,7 +2,6 @@ package org.mate.utils;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,13 +12,9 @@ import java.util.Map;
 public class MapUtils {
 
     public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+
         List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());
-        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
-            @Override
-            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
-                return o1.getValue().compareTo(o2.getValue());
-            }
-        });
+        Collections.sort(list, (o1, o2) -> o1.getValue().compareTo(o2.getValue()));
 
         Map<K, V> result = new LinkedHashMap<>();
         for (Map.Entry<K, V> entry : list) {
