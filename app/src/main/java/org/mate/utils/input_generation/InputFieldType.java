5646141b3ff510db4e757cd1cf865dcb91ad915c
Severin Primbs <primbs@fim.uni-passau.de>
2021-12-05 09:37:29 +0100

    time and date test

--- app/src/main/java/org/mate/utils/input_generation/InputFieldType.java
+++ app/src/main/java/org/mate/utils/input_generation/InputFieldType.java
@@ -1,9 +1,17 @@
 package org.mate.utils.input_generation;
 
+
 import android.text.InputType;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // TODO: add documentation
 public enum InputFieldType {
@@ -14,12 +22,12 @@ public enum InputFieldType {
     TEXT_VARIATION_POSTAL_ADDRESS("^[\\w-,ß\\s\\.,]{7,}", InputType.TYPE_TEXT_VARIATION_POSTAL_ADDRESS | InputType.TYPE_CLASS_TEXT),
     TEXT_VARIATION_EMAIL("[\\w\\._%+-]+\\@[\\w.-]+\\.[A-Za-z]{2,4}", InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS | InputType.TYPE_CLASS_TEXT),
     CLASS_PHONE("(\\(\\+[0-9]{2,3}\\)){0,1}\\p{javaSpaceChar}{0,1}[0-9]{3,}[/\\p{javaSpaceChar}-]{0,1}[0-9]{2,}", InputType.TYPE_CLASS_PHONE),
-    NUMBER_VARIATION_PASSWORD("^[0-9]+", InputType.TYPE_NUMBER_VARIATION_PASSWORD | InputType.TYPE_CLASS_NUMBER),
-    NUMBER_FLAG_SIGNED("[+|-]{1}[0-9]+\\.[0-9]+", InputType.TYPE_NUMBER_FLAG_SIGNED | InputType.TYPE_CLASS_NUMBER),
+    NUMBER_VARIATION_PASSWORD("^[0-9]{4,}", InputType.TYPE_NUMBER_VARIATION_PASSWORD | InputType.TYPE_CLASS_NUMBER),
+    NUMBER_FLAG_SIGNED("[+|-]{0,1}[0-9]+((\\.|,)[0-9]+){0,1}", InputType.TYPE_NUMBER_FLAG_SIGNED | InputType.TYPE_CLASS_NUMBER),
     CLASS_NUMBER("^[0-9]+", InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_CLASS_NUMBER),
-    NUMBER_FLAG_DECIMAL("[0-9]+\\.[0-9]+", InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_CLASS_NUMBER),
-    DATETIME_VARIATION_TIME("[0-9]{1,2}\\:[0-9]{2}", InputType.TYPE_DATETIME_VARIATION_TIME | InputType.TYPE_CLASS_DATETIME),
-    DATETIME_VARIATION_DATE("[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{4}", InputType.TYPE_DATETIME_VARIATION_DATE | InputType.TYPE_CLASS_DATETIME),
+    NUMBER_FLAG_DECIMAL("[0-9]+([\\.,][0-9]+){0,1}", InputType.TYPE_NUMBER_FLAG_DECIMAL | InputType.TYPE_CLASS_NUMBER),
+    DATETIME_VARIATION_TIME(InputType.TYPE_DATETIME_VARIATION_TIME | InputType.TYPE_CLASS_DATETIME),
+    DATETIME_VARIATION_DATE(InputType.TYPE_DATETIME_VARIATION_DATE | InputType.TYPE_CLASS_DATETIME),
     NOTHING;
 
 
@@ -27,7 +35,12 @@ public enum InputFieldType {
 
     private String regexInputField;
 
-    InputFieldType(){}
+    InputFieldType() {
+    }
+
+    InputFieldType(int inputTypeNumber) {
+        this.inputTypeNumber = inputTypeNumber;
+    }
 
     InputFieldType(String regexInputField, int inputTypeNumber) {
         this.regexInputField = regexInputField;
@@ -46,14 +59,98 @@ public enum InputFieldType {
     public static Set<InputFieldType> getInputFieldsMatchingRegex(final String staticString) {
         Set<InputFieldType> fields = new HashSet<>();
         for (InputFieldType inputField : values()) {
-            if(inputField!=NOTHING && staticString.matches(inputField.regexInputField)){
-                fields.add(inputField);
+            if (inputField.regexInputField != null) {
+                if (inputField != NOTHING && staticString.matches(inputField.regexInputField)) {
+                    fields.add(inputField);
+                }
+            } else if (inputField == DATETIME_VARIATION_DATE) {
+                if (isDate(staticString)) {
+                    fields.add(inputField);
+                }
+            } else if (inputField == DATETIME_VARIATION_TIME) {
+                if (isTime(staticString)) {
+                    fields.add(inputField);
+                }
             }
         }
         return fields;
     }
 
-    public String getRegex(){
+    public String getRegex() {
         return regexInputField;
+    }
+
+    public static boolean isDate(String dateStr) {
+        int counter = 0;
+        for (DateFormat d : DateFormat.values()) {
+            try {
+                DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(d.getPattern());
+                LocalDate.parse(dateStr, dateFormatter);
+            } catch (DateTimeParseException e) {
+                counter++;
+            }
+        }
+        return counter != DateFormat.values().length;
+
+    }
+
+    public static boolean isTime(String timeStr) {
+        int counter = 0;
+        for (TimeFormat d : TimeFormat.values()) {
+            try {
+                DateTimeFormatter dateFormatter;
+                if (d.getLocale() != null) {
+                    dateFormatter = DateTimeFormatter.ofPattern(d.getPattern(), d.getLocale());
+                } else {
+                    dateFormatter = DateTimeFormatter.ofPattern(d.getPattern());
+                }
+                LocalTime.parse(timeStr, dateFormatter);
+            } catch (DateTimeParseException e) {
+                counter++;
+            }
+        }
+        return counter != TimeFormat.values().length;
+
+    }
+
+    private enum DateFormat {
+        VARIANT1("MM-dd-yyyy"),
+        VARIANT2("MM/dd/yyyy"),
+        VARIANT3("dd-MM-yyyy"),
+        VARIANT4("dd/MM/yyyy"),
+        VARIANT5("dd.MM.yyyy"),
+        VARIANT6("yyyy-MM-dd");
+
+        private final String pattern;
+
+        DateFormat(String pattern) {
+            this.pattern = pattern;
+        }
+
+        public String getPattern() {
+            return pattern;
+        }
+    }
+
+    private enum TimeFormat {
+        VARIANT1("H:mm:ss", null),
+        VARIANT2("H:mm", null),
+        VARIANT3("h:mm a", Locale.ENGLISH);
+
+        private final String pattern;
+        private final Locale locale;
+
+        TimeFormat(String pattern, Locale locale) {
+            this.pattern = pattern;
+            this.locale = locale;
+        }
+
+        public String getPattern() {
+            return pattern;
+        }
+
+        public Locale getLocale() {
+            return locale;
+        }
     }
 }
--- app/src/test/java/org/mate/utils/input_generation/InputFieldTypeTest.java
+++ app/src/test/java/org/mate/utils/input_generation/InputFieldTypeTest.java
@@ -17,110 +17,184 @@ public class InputFieldTypeTest {
 
 
     @ParameterizedTest
-    @ValueSource(strings = {"Max Mustermann", "G. Milner","Hans-Xaver","max", "Stefan", "v. Goethe","Müller"})
+    @ValueSource(strings = {"Max Mustermann", "G. Milner", "Hans-Xaver", "max", "Stefan", "v. Goethe", "Müller"})
     void testRegexVariationPersonNameSuccessful(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_PERSON_NAME;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertTrue(m.matches());
+        assertPatternTrue(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"new\nLine", "new\ttab","Hello!","\r", "", "+","-","$","462",":","[b-z]*","of"})
+    @ValueSource(strings = {"new\nLine", "new\ttab", "Hello!", "\r", "", "+", "-", "$", "462", ":", "[b-z]*", "of"})
     void testRegexVariationPersonNameFail(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_PERSON_NAME;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertFalse(m.matches());
+        assertPatternFalse(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Hello it's \n a multi\nline\rtest!",".;:-+*","&h10)2"})
+    @ValueSource(strings = {"Hello it's \n a multi\nline\rtest!", ".;:-+*", "&h10)2"})
     void testRegexVariationMultiLineSuccessful(String text) {
         InputFieldType type = InputFieldType.TEXT_FLAG_MULTI_LINE;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertTrue(m.matches());
+        assertPatternTrue(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"of","\n","\n\n"})
+    @ValueSource(strings = {"of", "\n", "\n\n"})
     void testRegexVariationMultiLineFail(String text) {
         InputFieldType type = InputFieldType.TEXT_FLAG_MULTI_LINE;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertFalse(m.matches());
+        assertPatternFalse(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"password","password123","pw#10ef","goodJob1234;"})
+    @ValueSource(strings = {"password", "password123", "pw#10ef", "goodJob1234;"})
     void testRegexVariationPWSuccessful(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_PASSWORD;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertTrue(m.matches());
+        assertPatternTrue(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Test a","ab", "hello\nworld"})
+    @ValueSource(strings = {"Test a", "ab", "hello\nworld"})
     void testRegexVariationPWFail(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_PASSWORD;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertFalse(m.matches());
+        assertPatternFalse(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"max.muster@gmail.com","hello@test.de","fsinfo@fim.uni-passau.de","hel_lo123@hello.com"})
+    @ValueSource(strings = {"max.muster@gmail.com", "hello@test.de", "fsinfo@fim.uni-passau.de", "hel_lo123@hello.com"})
     void testRegexVariationEmailSuccessful(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_EMAIL;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertTrue(m.matches());
+        assertPatternTrue(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"max.muster@","@yahoo.de","xyz@gmail","abc@@bce.de","hello!@test.de","h@h@world.com","no_Valid"})
+    @ValueSource(strings = {"max.muster@", "@yahoo.de", "xyz@gmail", "abc@@bce.de", "hello!@test.de", "h@h@world.com", "no_Valid"})
     void testRegexVariationEmailFail(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_EMAIL;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertFalse(m.matches());
+        assertPatternFalse(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Innstraße 41, 94032 Passau", "Innstr. 41 \n 94032 Passau","Innstr-weg 41","Innstra","Innstr. 41a \n 94032 Passau \n GERMANY"})
+    @ValueSource(strings = {"Innstraße 41, 94032 Passau", "Innstr. 41 \n 94032 Passau", "Innstr-weg 41", "Innstra", "Innstr. 41a \n 94032 Passau \n GERMANY"})
     void testRegexVariationPostalAddressSuccessful(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_POSTAL_ADDRESS;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertTrue(m.matches());
+        assertPatternTrue(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"Innstr. @ Passau", "Innstr. #4","[regex]+","092203","Innstr"})
+    @ValueSource(strings = {"Innstr. @ Passau", "Innstr. #4", "[regex]+", "092203", "Innstr"})
     void testRegexVariationPostalAddressFail(String text) {
         InputFieldType type = InputFieldType.TEXT_VARIATION_POSTAL_ADDRESS;
-        Pattern p = Pattern.compile(type.getRegex());
-        Matcher m = p.matcher(text);
-        assertFalse(m.matches());
+        assertPatternFalse(text, type);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"085112345", "0852/3948", "0851 293948", "0832-1933", "(+49)852 2334", "(+432)232/323"})
+    void testRegexVariationPhoneSuccessful(String text) {
+        InputFieldType type = InputFieldType.CLASS_PHONE;
+        assertPatternTrue(text, type);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/8438", "0/323", "03939/3/3323", "(+49)/", "(+49)23423/", "9393/"})
+    void testRegexVariationPhoneFail(String text) {
+        InputFieldType type = InputFieldType.CLASS_PHONE;
+        assertPatternFalse(text, type);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"085112345","0852/3948","0851 293948", "0832-1933","(+49)852 2334","(+432)232/323"})
-    void testRegexVariationPhoneSuccessful(String text) {
-        InputFieldType type = InputFieldType.CLASS_PHONE;
+    @ValueSource(strings = {"085112345", "0000"})
+    void testRegexVariationNumberPWSuccessful(String text) {
+        InputFieldType type = InputFieldType.NUMBER_VARIATION_PASSWORD;
+        assertPatternTrue(text, type);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"/8438", "039"})
+    void testRegexVariationNumberPWFail(String text) {
+        InputFieldType type = InputFieldType.NUMBER_VARIATION_PASSWORD;
+        assertPatternFalse(text, type);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1.23", "-12,3", "14", "22.00000", "-2", "+234"})
+    void testRegexSignedNumberSuccessful(String text) {
+        InputFieldType type = InputFieldType.NUMBER_FLAG_SIGNED;
+        assertPatternTrue(text, type);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "1.2.3", ".", ".,"})
+    void testRegexSignedNumberFail(String text) {
+        InputFieldType type = InputFieldType.CLASS_NUMBER;
+        assertPatternFalse(text, type);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "123", "344"})
+    void testRegexClassNumberSuccessful(String text) {
+        InputFieldType type = InputFieldType.CLASS_NUMBER;
+        assertPatternTrue(text, type);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "1.0", "abc"})
+    void testRegexClassNumberFail(String text) {
+        InputFieldType type = InputFieldType.CLASS_NUMBER;
+        assertPatternFalse(text, type);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1.0", "1.23", "0,23", "1"})
+    void testRegexNumberFlagSuccessful(String text) {
+        InputFieldType type = InputFieldType.NUMBER_FLAG_DECIMAL;
+        assertPatternTrue(text, type);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1.0", "+1.02", "3.02."})
+    void testRegexNumberFlagFail(String text) {
+        InputFieldType type = InputFieldType.NUMBER_FLAG_DECIMAL;
+        assertPatternFalse(text, type);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"31.10.2000", "2000-07-22", "12-25-1999", "07-15-2011", "31/07/2001"})
+    void testDateFormatSuccessful(String text) {
+        assertTrue(InputFieldType.isDate(text));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"32.10.2000", "200-07-22", "12--25-1999", "07-15", "31.07/2001"})
+    void testDateFormatFail(String text) {
+        assertFalse(InputFieldType.isDate(text));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"23:08:30","22:00","6:00","10:01 AM"})
+    void testTimeFormatSuccessful(String text) {
+        assertTrue(InputFieldType.isTime(text));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"25:08:30","22:00.05","6","pm"})
+    void testTimeFormatFail(String text) {
+        assertFalse(InputFieldType.isTime(text));
+    }
+
+    private void assertPatternTrue(String text, InputFieldType type) {
         Pattern p = Pattern.compile(type.getRegex());
         Matcher m = p.matcher(text);
         assertTrue(m.matches());
     }
 
-    @ParameterizedTest
-    @ValueSource(strings = {"/8438","0/323","03939/3/3323","(+49)/","(+49)23423/","9393/"})
-    void testRegexVariationPhoneFail(String text) {
-        InputFieldType type = InputFieldType.CLASS_PHONE;
+    private void assertPatternFalse(String text, InputFieldType type) {
         Pattern p = Pattern.compile(type.getRegex());
         Matcher m = p.matcher(text);
         assertFalse(m.matches());
     }
+
 }
