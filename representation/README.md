14a10f0d1dfa40b73ae4f210e08068c1d11db40b
Ivan <iarcuschin@gmail.com>
2022-01-24 10:50:22 -0300

    Add test that launches target package Main Activity

--- representation/README.md
+++ representation/README.md
@@ -22,3 +22,16 @@ Output can be found at `./representation/build/outputs/apk/androidTest/debug/rep
 
 You can check that it has the proper target package name by executing:
 `aapt dump xmltree ./representation/build/outputs/apk/androidTest/debug/representation-debug-androidTest.apk AndroidManifest.xml | grep targetPackage`
+
+## How to change AUT's APK signature
+
+You can use the `apksigner` provided in the Android SDK and the default Android debug key.
+This command changes the APK in-place, so you might want to make a copy first.
+
+`apksigner sign --ks ~/.android/debug.keystore --ks-key-alias androiddebugkey --ks-pass pass:android --key-pass pass:android <path-to-APK>`
+
+## How to run
+
+- Install AUT's APK: `adb install <path-toAPK>`
+- Install the `androidTest` APK: `adb install ./representation/build/outputs/apk/androidTest/debug/representation-debug-androidTest.apk`
+- Run DynamicTest: `adb shell am instrument -w -e class 'org.mate.representation.DynamicTest' org.mate.representation.test/androidx.test.runner.AndroidJUnitRunner`
---
+++ representation/src/androidTest/java/org/mate/representation/DynamicTest.java
@@ -0,0 +1,53 @@
+package org.mate.representation;
+
+import android.app.Instrumentation;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class DynamicTest {
+
+    /**
+     * Launches Main activity of target package.
+     * @throws Exception if no Main activity is found for target package
+     */
+    @Before
+    public void setup() throws Exception {
+        Instrumentation instrumentation = InstrumentationRegistry.getInstrumentation();
+        instrumentation.setInTouchMode(true);
+
+        // Which package name are we targeting?
+        Context context = instrumentation.getTargetContext();
+        String targetPackageName = context.getPackageName();
+
+        // Launch main activity of target package
+        PackageManager pm = context.getPackageManager();
+        Intent intent = pm.getLaunchIntentForPackage(targetPackageName);
+        if (intent == null) {
+            throw new Exception(String.format("No Main Activity was found for package %s", targetPackageName));
+            // If facing this error, refer to: https://developer.android.com/reference/android/content/pm/PackageManager#getLaunchIntentForPackage(java.lang.String)
+        }
+
+        instrumentation.startActivitySync(intent);
+        instrumentation.waitForIdleSync();
+    }
+
+    @Test
+    public void run() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("org.mate.representation", appContext.getPackageName());
+    }
+}
