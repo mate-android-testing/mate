5a697af76d5ef2f42184701fa9878b7e9ccba0cc
Ivan <iarcuschin@gmail.com>
2022-02-02 13:47:44 -0300

    Do not throw exception in StaticStringParser if it can be avoided

--- client/src/main/java/org/mate/utils/input_generation/StaticStringsParser.java
+++ client/src/main/java/org/mate/utils/input_generation/StaticStringsParser.java
@@ -4,11 +4,9 @@ import android.util.Xml;
 
 import org.mate.commons.utils.MATELog;
 import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Set;
@@ -43,29 +41,32 @@ public final class StaticStringsParser {
         try {
             XmlPullParser parser = Xml.newPullParser();
             parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, true);
-            InputStream inputStream = new FileInputStream(new File(STATIC_STRINGS_XML_FILE));
-            parser.setInput(inputStream, null);
+            File staticStringsXMLFile = new File(STATIC_STRINGS_XML_FILE);
+            if (staticStringsXMLFile.exists()) {
+                InputStream inputStream = new FileInputStream(staticStringsXMLFile);
+                parser.setInput(inputStream, null);
 
-            Set<String> strings = new HashSet<>();
-            String className = null;
+                Set<String> strings = new HashSet<>();
+                String className = null;
 
-            while (parser.next() != XmlPullParser.END_DOCUMENT) {
-                if (parser.getEventType() == XmlPullParser.START_TAG) {
-                    if (parser.getName().equals("strings")) {
-                        strings = new HashSet<>();
-                        className = parser.getAttributeValue(null, "class");
-                    } else if (parser.getName().equals("string")) {
-                        strings.add(parser.getAttributeValue(null,"value"));
-                    }
-                } else if (parser.getEventType() == XmlPullParser.END_TAG) {
-                    if (parser.getName().equals("strings")) {
+                while (parser.next() != XmlPullParser.END_DOCUMENT) {
+                    if (parser.getEventType() == XmlPullParser.START_TAG) {
+                        if (parser.getName().equals("strings")) {
+                            strings = new HashSet<>();
+                            className = parser.getAttributeValue(null, "class");
+                        } else if (parser.getName().equals("string")) {
+                            strings.add(parser.getAttributeValue(null, "value"));
+                        }
+                    } else if (parser.getEventType() == XmlPullParser.END_TAG) {
+                        if (parser.getName().equals("strings")) {
                             staticStrings.add(className, strings);
+                        }
                     }
                 }
-            }
 
-            staticStrings.setInitialised(true);
-        } catch (XmlPullParserException | IOException e) {
+                staticStrings.setInitialised(true);
+            }
+        } catch (Exception e) {
             MATELog.log_warn("Couldn't parse the static string constants!");
             e.printStackTrace();
         }
