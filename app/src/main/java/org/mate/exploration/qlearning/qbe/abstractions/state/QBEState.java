4e58231095b54f467e6950f3b2112daa007e9e6d
Michael <auermich@fim.uni-passau.de>
2022-02-07 15:04:49 +0100

    added javadoc

--- app/src/main/java/org/mate/exploration/qlearning/qbe/abstractions/state/QBEState.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/abstractions/state/QBEState.java
@@ -17,29 +17,61 @@ import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toMap;
 import static java.util.stream.Collectors.toSet;
 
+/**
+ * Defines a state in the context of QBE.
+ */
 public final class QBEState extends StateSkeleton<QBEAction> implements State<QBEAction> {
 
+    /**
+     * The applicable actions in the current state.
+     */
     private final Set<QBEAction> actions;
+
+    /**
+     * A mapping from (widget type, widget depth) to its relative count.
+     */
     private final Map<String, Integer> featureMap;
+
+    /**
+     *
+     */
     private int numberOfComponents;
 
+    /**
+     * Initialises a new state.
+     *
+     * @param screenState The underlying screen state.
+     */
     public QBEState(final IScreenState screenState) {
         Objects.requireNonNull(screenState);
         featureMap = computeFeatureMap(screenState.getWidgets());
-        actions = Collections.unmodifiableSet(screenState.getActions().stream().map(QBEAction::new).collect(toSet()));
+        actions = Collections.unmodifiableSet(screenState.getActions().stream()
+                .map(QBEAction::new)
+                .collect(toSet()));
         numberOfComponents = featureMap.values().stream().mapToInt(i -> i).sum();
     }
 
+    /**
+     * Used to clone a state (copy constructor).
+     *
+     * @param state The state to be cloned.
+     */
     public QBEState(final QBEState state) {
         Objects.requireNonNull(state);
-        // actions is unmodifiable, so a shallow copy is ok.
         actions = state.actions;
-        // Strings and Integers are immutable so a shallow copy is ok.
         featureMap = new HashMap<>(state.featureMap);
         numberOfComponents = state.numberOfComponents;
     }
 
-    private static Map<String, Integer> computeFeatureMap(final List<Widget> widgets) {
+    /**
+     * Computes the feature map from the given list of widgets, which is constructed as follows:
+     * The widgets are grouped based on their type, e.g. button, and their depth in the ui tree
+     * and mapped to their relative count.
+     *
+     * @param widgets The list of widgets.
+     * @return Returns the computed feature map.
+     */
+    private Map<String, Integer> computeFeatureMap(final List<Widget> widgets) {
         final Map<String, Long> counts = widgets.stream()
                 .map(widget -> widget.getClazz() + "@" + widget.getDepth())
                 .collect(groupingBy(Function.identity(), counting()));
@@ -48,25 +80,49 @@ public final class QBEState extends StateSkeleton<QBEAction> implements State<QB
                 .collect(toMap(Map.Entry::getKey, e -> e.getValue().intValue()));
     }
 
+    /**
+     * Retrieves the actions applicable on the given state.
+     *
+     * @return Returns the applicable actions.
+     */
     public Set<QBEAction> getActions() {
         return Collections.unmodifiableSet(actions);
     }
 
+    /**
+     * 
+     * @return
+     */
     @Override
     protected int getNumberOfComponent() {
         return numberOfComponents;
     }
 
+    /**
+     * Returns the feature map.
+     *
+     * @return Returns the feature map.
+     */
     @Override
     public Map<String, Integer> getFeatureMap() {
         return Collections.unmodifiableMap(featureMap);
     }
 
+    /**
+     *
+     */
     public void addDummyComponent() {
         featureMap.merge("dummyComponent@-1", 1, (value, ignored) -> value + 1);
         ++numberOfComponents;
     }
 
+    /**
+     * Defines a custom string representation for a {@link QBEState}. Note that this format
+     * is reflected in the serialized transition system, see
+     * {@link org.mate.exploration.qlearning.qbe.transition_system.TransitionSystemSerializer}.
+     *
+     * @return Returns the string representation of the state.
+     */
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
