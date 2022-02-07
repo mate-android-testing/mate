80847432c421ad1147f8036c4b75568fffb68a2a
Michael <auermich@fim.uni-passau.de>
2022-02-07 11:47:27 +0100

    added javadoc

--- app/src/main/java/org/mate/exploration/qlearning/qbe/exploration/ExplorationStrategy.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/exploration/ExplorationStrategy.java
@@ -5,8 +5,20 @@ import org.mate.exploration.qlearning.qbe.interfaces.State;
 
 import java.util.Optional;
 
+/**
+ * Defines the interface for an exploration strategy in the context of QBE.
+ *
+ * @param <S> The generic state type.
+ * @param <A> The generic action type.
+ */
 @FunctionalInterface
 public interface ExplorationStrategy<S extends State<A>, A extends Action> {
 
+    /**
+     * Chooses the next action based on the current state.
+     *
+     * @param currentState The current state.
+     * @return Returns the action that should be applied next if there is such an action.
+     */
     Optional<A> chooseAction(S currentState);
 }
