0cc1ba1cd7b9d6b0e337967a8bf25fbab835592c
Michael <auermich@fim.uni-passau.de>
2022-02-07 13:16:35 +0100

    added javadoc

--- app/src/main/java/org/mate/exploration/qlearning/qbe/abstractions/app/QBEApplication.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/abstractions/app/QBEApplication.java
@@ -15,24 +15,57 @@ import static org.mate.interaction.UIAbstractionLayer.ActionResult;
 import static org.mate.interaction.UIAbstractionLayer.ActionResult.SUCCESS;
 import static org.mate.interaction.UIAbstractionLayer.ActionResult.SUCCESS_NEW_STATE;
 
+/**
+ * Provides the necessary functionality to interact with the AUT, e.g. executing an action and
+ * retrieving the current state.
+ */
 public final class QBEApplication implements Application<QBEState, QBEAction> {
 
+    /**
+     * Enables the interaction with the AUT.
+     */
     private final UIAbstractionLayer uiAbstractionLayer;
-    private TestCase currentTestcase = TestCase.newInitializedTestCase();
-    private int testcaseLength = 0;
 
+    /**
+     * The current test case that is constructed.
+     */
+    private TestCase currentTestCase = TestCase.newInitializedTestCase();
+
+    /**
+     * The current test case length.
+     */
+    private int testCaseLength = 0;
+
+    /**
+     * Initialises a new AUT.
+     *
+     * @param uiAbstractionLayer The interaction engine.
+     */
     public QBEApplication(final UIAbstractionLayer uiAbstractionLayer) {
         this.uiAbstractionLayer = Objects.requireNonNull(uiAbstractionLayer);
     }
 
+    /**
+     * Retrieves the current state the application is in.
+     *
+     * @return Returns the current state.
+     */
     @Override
     public QBEState getCurrentState() {
         return new QBEState(uiAbstractionLayer.getLastScreenState());
     }
 
+    /**
+     * Executes the given action.
+     *
+     * @param action The action to be executed.
+     * @return Returns a pair consisting of the new state and the action result.
+     */
     @Override
     public Pair<Optional<QBEState>, ActionResult> executeAction(final QBEAction action) {
-        ActionResult result = currentTestcase.updateTestCaseGetResult(action.getUiAction(), testcaseLength++);
+
+        ActionResult result = currentTestCase.updateTestCaseGetResult(action.getUiAction(), testCaseLength++);
+
         if (result == SUCCESS || result == SUCCESS_NEW_STATE) {
             /*
              * UiAbstractionLayer.executeAction(...) currently does not check whether a new state is
@@ -49,16 +82,18 @@ public final class QBEApplication implements Application<QBEState, QBEAction> {
         }
     }
 
+    /**
+     * Resets the AUT. As a side effect, coverage data is collected.
+     */
     @Override
     public void reset() {
-        if (testcaseLength > 0) {
-            final Chromosome<TestCase> chromosome = new Chromosome<>(currentTestcase);
+        if (testCaseLength > 0) {
+            final Chromosome<TestCase> chromosome = new Chromosome<>(currentTestCase);
             CoverageUtils.storeTestCaseChromosomeCoverage(chromosome);
             CoverageUtils.logChromosomeCoverage(chromosome);
-            currentTestcase.finish();
-            currentTestcase = TestCase.newInitializedTestCase();
-            testcaseLength = 0;
-
+            currentTestCase.finish();
+            currentTestCase = TestCase.newInitializedTestCase();
+            testCaseLength = 0;
         }
         uiAbstractionLayer.resetApp();
     }
