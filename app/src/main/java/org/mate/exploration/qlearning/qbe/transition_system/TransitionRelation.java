2924412f58bca854801550ecc230c721d1bc16f0
Michael <auermich@fim.uni-passau.de>
2022-02-09 18:18:30 +0100

    added javadoc

--- app/src/main/java/org/mate/exploration/qlearning/qbe/transition_system/TransitionRelation.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/transition_system/TransitionRelation.java
@@ -7,13 +7,43 @@ import java.util.Objects;
 
 import static org.mate.interaction.UIAbstractionLayer.ActionResult;
 
+/**
+ * Defines a transition in the Extended Labeled Transition System (ELTS), i.e. a connection from
+ * state v to state v' with action a.
+ *
+ * @param <S> The generic state type.
+ * @param <A> The generic action type.
+ */
 public final class TransitionRelation<S extends State<A>, A extends Action> {
 
+    /**
+     * The source state.
+     */
     public final S from;
+
+    /**
+     * The action that triggers the transition.
+     */
     public final A trigger;
+
+    /**
+     * The target state.
+     */
     public final S to;
+
+    /**
+     * The action result, i.e. success or failure.
+     */
     public final ActionResult actionResult;
 
+    /**
+     * Initialises a new transition.
+     *
+     * @param from The source state of the transition.
+     * @param trigger The action leading from the source to the target state.
+     * @param to The target state of the transition.
+     * @param actionResult The action result.
+     */
     public TransitionRelation(final S from, final A trigger, final S to, final ActionResult actionResult) {
         this.from = Objects.requireNonNull(from);
         this.trigger = Objects.requireNonNull(trigger);
@@ -21,10 +51,23 @@ public final class TransitionRelation<S extends State<A>, A extends Action> {
         this.actionResult = Objects.requireNonNull(actionResult);
     }
 
+    /**
+     * A copy constructor.
+     *
+     * @param ts The transition that should be cloned.
+     */
     public TransitionRelation(final TransitionRelation<S, A> ts) {
         this(ts.from, ts.trigger, ts.to, ts.actionResult);
     }
 
+    /**
+     * Checks for equality between two transitions. Two transitions are considered equal if they
+     * have the same source and target state as well as the same action.
+     *
+     * @param o The other transition.
+     * @return Returns {@code true} if the two transitions are equal, otherwise {@code false} is
+     *          returned.
+     */
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -33,11 +76,17 @@ public final class TransitionRelation<S extends State<A>, A extends Action> {
             return false;
         } else {
             final TransitionRelation<?, ?> other = (TransitionRelation<?, ?>) o;
-            return from.equals(other.from) && trigger.equals(other.trigger) && Objects.equals(to,
-                    other.to);
+            return from.equals(other.from)
+                    && trigger.equals(other.trigger)
+                    && Objects.equals(to, other.to);
         }
     }
 
+    /**
+     * Computes the hash code for the given transition.
+     *
+     * @return Returns the hash code associated with the given transition.
+     */
     @Override
     public int hashCode() {
         int result = from.hashCode();
@@ -46,8 +95,14 @@ public final class TransitionRelation<S extends State<A>, A extends Action> {
         return result;
     }
 
+    /**
+     * Provides a custom string representation for the transition.
+     *
+     * @return Returns the string representation of the given transition.
+     */
     @Override
     public String toString() {
-        return "{\"from\":" + from + ",\"trigger\":" + trigger + ",\"to\":" + to + ",\"actionResult\":\"" + actionResult + "\"}";
+        return "{\"from\":" + from + ",\"trigger\":" + trigger + ",\"to\":" + to
+                + ",\"actionResult\":\"" + actionResult + "\"}";
     }
 }
--- app/src/main/java/org/mate/exploration/qlearning/qbe/transition_system/TransitionSystem.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/transition_system/TransitionSystem.java
@@ -10,24 +10,32 @@ import java.util.Set;
 
 import static java.util.stream.Collectors.toSet;
 
+/**
+ * Defines the Extended Labeled Transition System (ELTS).
+ *
+ * @param <S> The generic state type.
+ * @param <A> The generic action type.
+ */
 public final class TransitionSystem<S extends State<A>, A extends Action> {
 
+    /**
+     * The initial state v0.
+     */
     private final S initialState;
 
-    public Set<S> getStates() {
-        return states;
-    }
-
-    public Set<A> getActions() {
-        return actions;
-    }
-
-    public Set<TransitionRelation<S, A>> getTransitions() {
-        return transitions;
-    }
-
+    /**
+     * The set of states.
+     */
     private final Set<S> states = new HashSet<>();
+
+    /**
+     * The set of actions.
+     */
     private final Set<A> actions = new HashSet<>();
+
+    /**
+     * The set of transitions.
+     */
     private final Set<TransitionRelation<S, A>> transitions = new HashSet<>();
 
     public TransitionSystem(final S initialState) {
@@ -36,14 +44,55 @@ public final class TransitionSystem<S extends State<A>, A extends Action> {
         actions.addAll(initialState.getActions());
     }
 
+    /**
+     * Returns the initial state v0.
+     *
+     * @return Returns the initial state.
+     */
     public S getInitialState() {
         return initialState;
     }
 
+    /**
+     * Returns the states in the given transition system.
+     *
+     * @return Returns the states.
+     */
+    public Set<S> getStates() {
+        return states;
+    }
+
+    /**
+     * Returns the actions in the given transition system.
+     *
+     * @return Returns the actions.
+     */
+    public Set<A> getActions() {
+        return actions;
+    }
+
+    /**
+     * Returns the transitions in the given transition system.
+     *
+     * @return Returns the transitions.
+     */
+    public Set<TransitionRelation<S, A>> getTransitions() {
+        return transitions;
+    }
+
+    /**
+     * Adds a new transition to the ELTS.
+     *
+     * @param transitionRelation The new transition.
+     * @return Returns {@code true} if the new transition is non-deterministic, otherwise
+     *          {@code false} is returned.
+     */
     public boolean addTransition(final TransitionRelation<S, A> transitionRelation) {
+
         states.add(transitionRelation.from);
         actions.addAll(transitionRelation.from.getActions());
         actions.add(transitionRelation.trigger);
+
         if (transitionRelation.to != null) {
             states.add(transitionRelation.to);
             actions.addAll(transitionRelation.to.getActions());
@@ -56,27 +105,54 @@ public final class TransitionSystem<S extends State<A>, A extends Action> {
         return nonDeterministicTransitionRelation;
     }
 
+    /**
+     * Removes the given transition from the ELTS.
+     *
+     * @param transitionRelation The transition that should be removed.
+     */
     public void removeTransition(final TransitionRelation<S, A> transitionRelation) {
         transitions.remove(transitionRelation);
     }
 
+    /**
+     * Adds a set of actions to the ELTS.
+     *
+     * @param actions The set of actions that should be added.
+     */
     public void addActions(final Collection<A> actions) {
         this.actions.addAll(actions);
     }
 
+    /**
+     * Retrieves the actions that are applicable in the given state.
+     *
+     * @param state The given state.
+     * @return Returns the applicable actions in the given state.
+     */
+    @SuppressWarnings("unused")
     public Set<A> nextActions(final S state) {
         return transitions.stream().filter(transitions -> transitions.from.equals(state))
                 .map(transition -> transition.trigger).collect(toSet());
     }
 
+    /**
+     * Retrieves the transitions that define the given source state and the given action.
+     *
+     * @param state The source state.
+     * @param action The action.
+     * @return Returns the transitions that match the given source state and action.
+     */
     public Set<TransitionRelation<S, A>> nextStates(final S state, final A action) {
         return transitions.stream()
                 .filter(transition -> transition.from.equals(state) && transition.trigger.equals(action))
                 .collect(toSet());
     }
 
+    /**
+     * Removes the unreachable states.
+     */
     public void removeUnreachableStates() {
-        // Todo: This can probably be implemented more efficiently.
+        // TODO: This can probably be implemented more efficiently.
         final Set<TransitionRelation<S, A>> reachableTransitions = new HashSet<>(transitions.size());
         final Set<S> reachableStates = new HashSet<>(states.size());
         reachableStates.add(initialState);
@@ -98,6 +174,12 @@ public final class TransitionSystem<S extends State<A>, A extends Action> {
         actions.retainAll(reachableStates.stream().flatMap(s -> s.getActions().stream()).collect(toSet()));
     }
 
+    /**
+     * Checks whether the ELTS is deterministic. An ELTS is deterministic iff:
+     *      ∀ v,v',v'' ∈ V, ∀z ∈ λ(v): [(v, v', z) ∈ ω and (v, v'', z) ∈ ω] → (v' = v'')
+     *
+     * @return Returns {@code true} if the ELTS is deterministic, otherwise {@code false} is returned.
+     */
     public boolean isDeterministic() {
         for (final TransitionRelation<S, A> transition1 : transitions) {
             for (final TransitionRelation<S, A> transition2 : transitions) {
@@ -111,6 +193,11 @@ public final class TransitionSystem<S extends State<A>, A extends Action> {
         return true;
     }
 
+    /**
+     * Defines a custom string representation of the ELTS.
+     *
+     * @return Returns the string representation of the ELTS.
+     */
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder();
@@ -130,5 +217,4 @@ public final class TransitionSystem<S extends State<A>, A extends Action> {
         sb.append("]}");
         return sb.toString();
     }
-
 }
