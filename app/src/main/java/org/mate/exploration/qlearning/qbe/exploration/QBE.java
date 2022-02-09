b5467655a7437a5e1d360297c4f33f93dcefa5fe
Michael <auermich@fim.uni-passau.de>
2022-02-09 17:21:57 +0100

    added javadoc

--- app/src/main/java/org/mate/exploration/qlearning/qbe/exploration/QBE.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/exploration/QBE.java
@@ -18,21 +18,46 @@ import java.util.stream.Collectors;
 import static org.mate.utils.Randomness.getDistributedRandomNumber;
 import static org.mate.utils.StreamUtils.distinctByKey;
 
+/**
+ * Provides a simple Q-Learning based exploration strategy as outlined in Algorithm 3.
+ *
+ * @param <S> The generic state type.
+ * @param <A> The generic action type.
+ */
 public final class QBE<S extends State<A>, A extends Action> implements ExplorationStrategy<S, A> {
 
+    /**
+     * The transition prioritization matrix or shortly denoted as q-matrix.
+     */
     private final QMatrix<S, A> qmatrix;
+
+    /**
+     * The set of abstract actions.
+     */
     private final QMatrix.AbstractActions<A> abstractActions;
 
-
+    /**
+     * Initialises the Q-Learning based exploration strategy with the given q-matrix.
+     *
+     * @param qmatrix The given q-matrix.
+     */
     public QBE(final QMatrix<S, A> qmatrix) {
         this.qmatrix = Objects.requireNonNull(qmatrix);
         this.abstractActions = qmatrix.getActionLabelingFunction();
     }
 
+    /**
+     * Chooses the next action, see Algorithm 3 for more details.
+     *
+     * @param currentState The current state.
+     * @return Returns the action that should be applied next.
+     */
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public Optional<A> chooseAction(final S currentState) {
+
         final Set<A> currentActions = currentState.getActions();
+
         if (currentActions.isEmpty()) {
             return Optional.empty();
         } else {
--- app/src/main/java/org/mate/exploration/qlearning/qbe/exploration/RandomExploration.java
+++ app/src/main/java/org/mate/exploration/qlearning/qbe/exploration/RandomExploration.java
@@ -7,11 +7,24 @@ import org.mate.utils.Randomness;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * Provides a random exploration strategy.
+ *
+ * @param <S> The generic state type.
+ * @param <A> The generic action type.
+ */
 public final class RandomExploration<S extends State<A>, A extends Action> implements ExplorationStrategy<S, A> {
 
+    /**
+     * Chooses a random action from the current state.
+     *
+     * @param currentState The current state.
+     * @return Returns a random action applicable on the current state if possible.
+     */
     @Override
     public Optional<A> chooseAction(final S currentState) {
         final Set<A> possibleActions = currentState.getActions();
-        return possibleActions.isEmpty() ? Optional.empty() : Optional.of(Randomness.randomElement(possibleActions));
+        return possibleActions.isEmpty()
+                ? Optional.empty() : Optional.of(Randomness.randomElement(possibleActions));
     }
 }
