b3645b3164e63bf059f44e87713de2a24b994c71
Michael <auermich@fim.uni-passau.de>
2022-03-24 13:26:51 +0100

    added abstract episodic exploration

--- app/src/main/java/org/mate/exploration/rl/qlearning/autodroid/EpisodicExploration.java
+++ app/src/main/java/org/mate/exploration/rl/qlearning/autodroid/EpisodicExploration.java
@@ -1,8 +1,12 @@
 package org.mate.exploration.rl.qlearning.autodroid;
 
+import org.mate.MATE;
 import org.mate.Registry;
 import org.mate.exploration.Algorithm;
 import org.mate.interaction.UIAbstractionLayer;
+import org.mate.model.IGUIModel;
+import org.mate.state.IScreenState;
+import org.mate.utils.Randomness;
 
 /**
  * An implementation based on the paper 'Reinforcement Learning for Android GUI Testing', see
@@ -21,6 +25,11 @@ public class EpisodicExploration implements Algorithm {
     private final UIAbstractionLayer uiAbstractionLayer = Registry.getUiAbstractionLayer();
 
     /**
+     * The current gui model.
+     */
+    private final IGUIModel guiModel;
+
+    /**
      * The maximal number of episodes.
      */
     private final int maxNumOfEpisodes;
@@ -36,11 +45,36 @@ public class EpisodicExploration implements Algorithm {
     public EpisodicExploration(int maxNumOfEpisodes, int maxEpisodeLength, float initialQValue,
                                float pHomeButton) {
         this.maxNumOfEpisodes = maxNumOfEpisodes;
+        this.guiModel = uiAbstractionLayer.getGuiModel();
         autoDroidChromosomeFactory = new AutoDroidChromosomeFactory(maxEpisodeLength, initialQValue, pHomeButton);
     }
 
+    /**
+     * Explores the AUT in episodes until the maximal number of episodes is reached or the specified
+     * timeout is triggered.
+     */
     @Override
     public void run() {
 
+        for (int i = 0; i < maxNumOfEpisodes; i++) {
+            MATE.log_acc("Episode #" + (i + 1));
+
+            /*
+            * We start an episode in a random state. If we couldn't reach the randomly selected state,
+            * we start the episode in that state.
+             */
+            IScreenState randomScreenState = Randomness.randomElement(guiModel.getAppStates());
+            boolean success = uiAbstractionLayer.moveToState(randomScreenState);
+
+            MATE.log_acc("Starting episode in random state: " + success);
+            MATE.log_acc("Starting episode in activity: " + uiAbstractionLayer.getCurrentActivity());
+
+            if (!uiAbstractionLayer.isAppOpened()) {
+                // start episode from main activity
+                uiAbstractionLayer.restartApp();
+            }
+
+            autoDroidChromosomeFactory.createChromosome();
+        }
     }
 }
