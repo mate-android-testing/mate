9a5447db114b839c5f53fa87e66e2b61e496b70c
Michael <auermich@fim.uni-passau.de>
2022-03-25 12:04:30 +0100

    added TODO, see #91

--- app/src/main/java/org/mate/interaction/UIAbstractionLayer.java
+++ app/src/main/java/org/mate/interaction/UIAbstractionLayer.java
@@ -480,6 +480,13 @@ public class UIAbstractionLayer {
         Utils.sleep(5000);
         deviceMgr.restartApp();
         Utils.sleep(2000);
+
+        /*
+         * TODO: Try to merge different start screen states. If the restart leads to a different
+         *  start screen state (this happens sporadically), we introduce an isolated subgraph in the
+         *  gui model with the next update call. Another possible fix is to introduce an dedicated
+         *  restart action that then connects the subgraph through a restart edge.
+         */
         lastScreenState = toRecordedScreenState(clearScreen());
     }
 
@@ -489,6 +496,13 @@ public class UIAbstractionLayer {
     public void restartApp() {
         deviceMgr.restartApp();
         Utils.sleep(2000);
+
+        /*
+         * TODO: Try to merge different start screen states. If the restart leads to a different
+         *  start screen state (this happens sporadically), we introduce an isolated subgraph in the
+         *  gui model with the next update call. Another possible fix is to introduce an dedicated
+         *  restart action that then connects the subgraph through a restart edge.
+         */
         lastScreenState = toRecordedScreenState(clearScreen());
     }
 
