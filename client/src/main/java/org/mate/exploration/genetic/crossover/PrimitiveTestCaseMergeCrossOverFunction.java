32c474cf1eae3290dbfc27726f0cb7361559928e
Michael <auermich@fim.uni-passau.de>
2022-06-28 22:18:22 +0200

    turn on execution of test case after crossover by default

--- client/src/main/java/org/mate/exploration/genetic/crossover/PrimitiveTestCaseMergeCrossOverFunction.java
+++ client/src/main/java/org/mate/exploration/genetic/crossover/PrimitiveTestCaseMergeCrossOverFunction.java
@@ -30,7 +30,7 @@ public class PrimitiveTestCaseMergeCrossOverFunction implements ICrossOverFuncti
      * chromosome factory.
      */
     public PrimitiveTestCaseMergeCrossOverFunction() {
-        executeActions = false;
+        executeActions = true;
     }
 
     /**
