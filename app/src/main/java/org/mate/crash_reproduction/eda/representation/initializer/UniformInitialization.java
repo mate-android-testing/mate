6ffb8e58ec3fbad8a34cb313d5132d4cec056fa3
Dominik Diner <diner@fim.uni-passau.de>
2022-07-05 18:11:38 +0200

    Added uniform initialization

---
+++ app/src/main/java/org/mate/crash_reproduction/eda/representation/initializer/UniformInitialization.java
@@ -0,0 +1,21 @@
+package org.mate.crash_reproduction.eda.representation.initializer;
+
+import org.mate.interaction.action.Action;
+import org.mate.interaction.action.ui.UIAction;
+import org.mate.state.IScreenState;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class UniformInitialization implements BiFunction<List<Action>, IScreenState, Map<Action, Double>> {
+    @Override
+    public Map<Action, Double> apply(List<Action> actions, IScreenState screenState) {
+        Set<UIAction> uniqueActions = new HashSet<>(screenState.getActions());
+        return uniqueActions.stream().collect(Collectors.toMap(Function.identity(), a -> 1D / uniqueActions.size()));
+    }
+}
