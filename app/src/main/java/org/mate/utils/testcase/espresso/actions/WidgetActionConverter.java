968c838daa8fd52715f61cbcd2c8acf33732e4af
Michael <auermich@fim.uni-passau.de>
2022-07-26 21:09:56 +0200

    added view action construction for widget actions

--- app/src/main/java/org/mate/utils/testcase/espresso/actions/WidgetActionConverter.java
+++ app/src/main/java/org/mate/utils/testcase/espresso/actions/WidgetActionConverter.java
@@ -6,16 +6,41 @@ import org.mate.Registry;
 import org.mate.interaction.action.ui.ActionType;
 import org.mate.interaction.action.ui.Widget;
 import org.mate.interaction.action.ui.WidgetAction;
+import org.mate.utils.testcase.espresso.EspressoDependency;
+
+import java.util.EnumSet;
 
 import static org.mate.utils.testcase.espresso.EspressoDependency.ALL_OF;
+import static org.mate.utils.testcase.espresso.EspressoDependency.CLEAR_TEXT;
+import static org.mate.utils.testcase.espresso.EspressoDependency.CLOSE_SOFT_KEYBOARD;
+import static org.mate.utils.testcase.espresso.EspressoDependency.SCROLL_TO;
 import static org.mate.utils.testcase.espresso.EspressoDependency.WITH_TEXT;
 
+/**
+ * Converts a {@link WidgetAction} to an espresso action.
+ */
 public class WidgetActionConverter extends ActionConverter {
 
+    /**
+     * The widget on which an action should be applied.
+     */
     private final Widget widget;
+
+    /**
+     * The type of action that should be applied.
+     */
     private final ActionType actionType;
+
+    /**
+     * Stores the number of applicable view matchers.
+     */
     private final int numberOfUsableViewMatchers;
 
+    /**
+     * Constructs a converter for the given widget action.
+     *
+     * @param action The widget action that should be converted to an espresso action.
+     */
     public WidgetActionConverter(WidgetAction action) {
         super(action);
         this.widget = action.getWidget();
@@ -23,6 +48,12 @@ public class WidgetActionConverter extends ActionConverter {
         this.numberOfUsableViewMatchers = getNumberOfUsableViewMatchers();
     }
 
+    /**
+     * Returns a short form of the resource id.
+     *
+     * @return Returns a short form of the resource id or {@code null} if the resource id doesn't
+     *         start with the package name or isn't valid.
+     */
     private String getShortResourceID() {
 
         // the resource id might look as follows: android:id/statusBarBackground
@@ -53,8 +84,91 @@ public class WidgetActionConverter extends ActionConverter {
      */
     @Override
     protected void buildPerform() {
+
+        builder.append(".perform(");
+
+        EnumSet<ActionType> textActions = EnumSet.of(ActionType.CLEAR_TEXT,
+                ActionType.TYPE_SPECIFIC_TEXT, ActionType.TYPE_TEXT);
+
+        if (textActions.contains(actionType)) {
+            // we need to close the soft keyboard first
+            builder.append(CLOSE_SOFT_KEYBOARD).append("()").append(", ");
+        }
+
+        if (widget.isSonOfScrollView()) {
+            builder.append(SCROLL_TO).append("()").append(", ");
+        }
+
+        // remove any text before we insert some new text
+        if (actionType == ActionType.TYPE_TEXT || actionType == ActionType.TYPE_SPECIFIC_TEXT) {
+            builder.append(CLEAR_TEXT).append("()").append(", ");
+        }
+
+        // insert the actual action
+        buildAction();
+
+        if (textActions.contains(actionType)) {
+            // close the soft keyboard in case we inserted some new text
+            builder.append(", ").append(CLOSE_SOFT_KEYBOARD).append("()");
+        }
+
+        // close 'perform()' clause
+        builder.append(");");
     }
 
+    /**
+     * Builds the actual action together.
+     */
+    private void buildAction() {
+
+        EspressoDependency dependency;
+
+        switch (actionType) {
+            case CLICK:
+                dependency = EspressoDependency.CLICK;
+                break;
+            case LONG_CLICK:
+                dependency = EspressoDependency.LONG_CLICK;
+                break;
+            case TYPE_TEXT:
+            case TYPE_SPECIFIC_TEXT:
+                dependency = EspressoDependency.TYPE_TEXT;
+                break;
+            case CLEAR_TEXT:
+                dependency = CLEAR_TEXT;
+                break;
+            case SWIPE_LEFT:
+                dependency = EspressoDependency.SWIPE_LEFT;
+                break;
+            case SWIPE_RIGHT:
+                dependency = EspressoDependency.SWIPE_RIGHT;
+                break;
+            case SWIPE_DOWN:
+                dependency = EspressoDependency.SWIPE_DOWN;
+                break;
+            case SWIPE_UP:
+                dependency = EspressoDependency.SWIPE_UP;
+                break;
+            default:
+                throw new UnsupportedOperationException("Action type " + actionType
+                        + " not yet supported");
+        }
+
+        builder.append(dependency).append("(");
+
+        // the text actions requires the text to insert as additional argument
+        if (dependency == EspressoDependency.TYPE_TEXT) {
+            builder.append("\"");
+            builder.append(widget.getText());
+            builder.append("\"");
+        }
+
+        builder.append(")");
+    }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     protected void buildViewMatcher() {
 
@@ -115,8 +229,8 @@ public class WidgetActionConverter extends ActionConverter {
      * Builds a view matcher for a text view using the custom 'oneOf()' matcher.
      */
     private void buildOneOf() {
-            builder.append("oneOf(");
-            buildTextMatcher(true);
+        builder.append("oneOf(");
+        buildTextMatcher(true);
     }
 
     /**
@@ -135,11 +249,11 @@ public class WidgetActionConverter extends ActionConverter {
      * Builds a view matcher using the 'allOf()' matcher, see the hamcrest API for more details.
      */
     private void buildAllOf() {
-            builder.append(ALL_OF).append("(");
-            buildCoordinatesMatcher();
-            buildClassMatcher();
-            buildTextMatcher(false);
-            builder.append(")");
+        builder.append(ALL_OF).append("(");
+        buildCoordinatesMatcher();
+        buildClassMatcher();
+        buildTextMatcher(false);
+        builder.append(")");
     }
 
     /**
@@ -159,6 +273,7 @@ public class WidgetActionConverter extends ActionConverter {
      */
     private void buildClassMatcher() {
         String className = getShortClassName();
+        // TODO: Why can't we build a class matcher for an image view?
         if (className != null && !className.equals("ImageView")) {
             builder.append(", ").append("withClassNameString(\"").append(className).append("\")");
         }
@@ -172,6 +287,7 @@ public class WidgetActionConverter extends ActionConverter {
     private boolean isNonEmptyTextView() {
         // TODO: Is the class check necessary or can it be any kind of text view?
         return widget.getClazz().equals(TextView.class.getName())
+                // TODO: Should we consider the content description attribute as well?
                 && widget.getText() != null
                 && !widget.getText().isEmpty();
     }
