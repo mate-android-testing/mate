aed511ce75bccbd370bdfbd70a4df4c626717e27
Michael <auermich@fim.uni-passau.de>
2022-07-27 12:16:54 +0200

    added documentation

--- app/src/main/java/org/mate/utils/testcase/espresso/actions/ActionConverter.java
+++ app/src/main/java/org/mate/utils/testcase/espresso/actions/ActionConverter.java
@@ -4,6 +4,9 @@ import org.mate.interaction.action.Action;
 
 import static org.mate.utils.testcase.espresso.EspressoDependency.ON_VIEW;
 
+/**
+ * Provides an abstract converter for an {@link Action}.
+ */
 public abstract class ActionConverter {
 
     /**
@@ -11,12 +14,25 @@ public abstract class ActionConverter {
      */
     protected final Action action;
 
+    /**
+     * The internal builder containing the final espresso action sequence.
+     */
     protected final StringBuilder builder = new StringBuilder();
 
+    /**
+     * Constructs a converter for the given action.
+     *
+     * @param action The action that should be converted.
+     */
     public ActionConverter(Action action) {
         this.action = action;
     }
 
+    /**
+     * Performs the conversion process.
+     *
+     * @return Returns the obtained espresso action.
+     */
     public String convert() {
         openViewMatcher();
         buildViewMatcher();
@@ -26,18 +42,33 @@ public abstract class ActionConverter {
         return builder.toString();
     }
 
+    /**
+     * Builds a comment.
+     */
     protected void buildComment() {
         builder.append("// ");
     }
 
+    /**
+     * Builds the view action.
+     */
     protected abstract void buildPerform();
 
+    /**
+     * Closes the view matcher.
+     */
     protected void closeViewMatcher() {
         builder.append(")");
     }
 
+    /**
+     * Builds the view matcher.
+     */
     protected abstract void buildViewMatcher();
 
+    /**
+     * Opens the view matcher.
+     */
     protected void openViewMatcher() {
         builder.append(ON_VIEW).append("(");
     }
