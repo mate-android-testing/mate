6d9ee599d43a5c5517ff6f91cf8ab6744519fef6
Dominik Diner <diner@fim.uni-passau.de>
2022-08-18 12:23:30 +0200

    Check before executing crash reproduction specific code

--- app/src/main/java/org/mate/model/TestCase.java
+++ app/src/main/java/org/mate/model/TestCase.java
@@ -159,10 +159,10 @@ public class TestCase {
 
         if (Properties.TARGET().equals("stack_trace")) {
             MATE.log(nickName + " Testcase fitness: " + new CrashDistance().getFitness(chromosome));
+            Registry.getEnvironmentManager().logReachedTargets(chromosome);
         }
         MATE.log(nickName + " Testcase reached activities: [" + visitedActivities.stream().collect(Collectors.joining(", ")) + "]");
         MATE.log(nickName + " Testcase reached fragments: [" + visitedFragments.stream().collect(Collectors.joining(", "))  + "]");
-        Registry.getEnvironmentManager().logReachedTargets(chromosome);
         // TODO: log the test case actions in a proper format
     }
 
