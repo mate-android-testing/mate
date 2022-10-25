2be725ecb5887e3ea2199bcc84e3a2e820777512
Michael <auermich@fim.uni-passau.de>
2022-10-25 10:54:27 +0200

    added documentation

--- app/src/main/java/org/mate/utils/UIAutomatorException.java
+++ app/src/main/java/org/mate/utils/UIAutomatorException.java
@@ -4,9 +4,17 @@ package org.mate.utils;
  * Thrown when an UIAutomator appears.
  */
 public class UIAutomatorException extends RuntimeException {
+
+    /**
+     * Constructs an UIAutomator exception.
+     *
+     * @param message The error message.
+     * @param cause The cause for this exception.
+     */
     public UIAutomatorException(String message, Throwable cause) {
         super(message, cause);
     }
+
     /**
      * Constructs an UIAutomator exception.
      *
