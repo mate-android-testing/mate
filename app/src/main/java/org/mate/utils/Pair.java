d6cf1abfb4e3b9b7d0d22a329e86a3bb19ee2c9d
Michael <auermich@fim.uni-passau.de>
2023-03-21 11:22:50 +0100

    added documentation

--- app/src/main/java/org/mate/utils/Pair.java
+++ app/src/main/java/org/mate/utils/Pair.java
@@ -2,11 +2,30 @@ package org.mate.utils;
 
 import java.util.Objects;
 
+/**
+ * Provides a generic pair class.
+ *
+ * @param <T> The type of the first entry.
+ * @param <U> The type of the second entry.
+ */
 public final class Pair<T, U> {
 
+    /**
+     * The first entry of the pair.
+     */
     public final T first;
+
+    /**
+     * The second entry of the pair.
+     */
     public final U second;
 
+    /**
+     * Initialises a new pair with the given first and second entry.
+     *
+     * @param first The first entry of the pair.
+     * @param second The second entry of the pair.
+     */
     public Pair(final T first, final U second) {
         this.first = Objects.requireNonNull(first);
         this.second = Objects.requireNonNull(second);
