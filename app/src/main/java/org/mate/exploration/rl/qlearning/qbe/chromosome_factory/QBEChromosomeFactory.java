d70c1079f937e94a16d3da215147c9357b40f34c
Michael Pusl <pusl03@ads.uni-passau.de>
2023-03-30 16:58:03 +0200

    Fix: Choose random action if action is not applicable.

--- app/src/main/java/org/mate/exploration/rl/qlearning/qbe/chromosome_factory/QBEChromosomeFactory.java
+++ app/src/main/java/org/mate/exploration/rl/qlearning/qbe/chromosome_factory/QBEChromosomeFactory.java
@@ -1,10 +1,16 @@
 package org.mate.exploration.rl.qlearning.qbe.chromosome_factory;
 
+import org.mate.MATE;
 import org.mate.Properties;
+import org.mate.Registry;
 import org.mate.exploration.genetic.chromosome_factory.AndroidRandomChromosomeFactory;
 import org.mate.exploration.rl.qlearning.qbe.exploration.ExplorationStrategy;
 import org.mate.interaction.action.Action;
+import org.mate.interaction.action.ui.UIAction;
 import org.mate.model.fsm.qbe.QBEModel;
+import org.mate.utils.Randomness;
+
+import java.util.List;
 
 public class QBEChromosomeFactory extends AndroidRandomChromosomeFactory {
 
@@ -21,6 +27,12 @@ public class QBEChromosomeFactory extends AndroidRandomChromosomeFactory {
 
     @Override
     protected Action selectAction() {
-        return explorationStrategy.chooseAction(model.getCurrentState()).get();
+        UIAction chosen = (UIAction) explorationStrategy.chooseAction(model.getCurrentState()).get();
+        List<UIAction> applicableActions = Registry.getUiAbstractionLayer().getExecutableUIActions();
+        if (applicableActions.contains(chosen))
+            return chosen;
+
+        MATE.log_acc(String.format("Chosen action %s not applicable to current state.", chosen));
+        return Randomness.randomElement(applicableActions);
     }
 }
