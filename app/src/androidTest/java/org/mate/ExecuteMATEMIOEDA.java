ef2792f80f1caaa9ec3aa866b1699ca2c9262f5e
Andreas Strobl <strobland@fim.uni-passau.de>
2023-10-14 11:58:27 +0200

    initial commit

---
+++ app/src/androidTest/java/org/mate/ExecuteMATEMIOEDA.java
@@ -0,0 +1,45 @@
+package org.mate;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mate.exploration.genetic.algorithm.Algorithm;
+import org.mate.exploration.genetic.builder.GeneticAlgorithmBuilder;
+import org.mate.exploration.genetic.core.IGeneticAlgorithm;
+
+@RunWith(AndroidJUnit4.class)
+public class ExecuteMATEMIOEDA {
+
+
+    @Test
+    public void useAppContext() {
+        MATE.log_acc("Starting Evolutionary Search...");
+        MATE.log_acc("MIO algorithm");
+
+        MATE mate = new MATE();
+
+        GeneticAlgorithmBuilder builder = new GeneticAlgorithmBuilder()
+                .withAlgorithm(Algorithm.MIO)
+                .withChromosomeFactory(Properties.CHROMOSOME_FACTORY())
+                .withMutationFunction(Properties.MUTATION_FUNCTION())
+                .withTerminationCondition(Properties.TERMINATION_CONDITION())
+                .withPopulationSize(Properties.POPULATION_SIZE())
+                .withBigPopulationSize(Properties.BIG_POPULATION_SIZE())
+                .withPMutate(Properties.P_MUTATE())
+                .withPCrossover(Properties.P_CROSSOVER())
+                .withPSampleRandom(Properties.P_SAMPLE_RANDOM())
+                .withFocusedSearchStart(Properties.P_FOCUSED_SEARCH_START())
+                .withMutationRate(Properties.MUTATION_RATE());
+
+        int numberOfObjectives
+                = Registry.getEnvironmentManager().getNumberOfObjectives(Properties.OBJECTIVE());
+
+        // we need to associate with each objective (branch, line) a fitness function
+        builder = builder.withFitnessFunctions(Properties.FITNESS_FUNCTION(), numberOfObjectives);
+
+        final IGeneticAlgorithm mio = builder.build();
+        mate.testApp(mio);
+    }
+}
+
