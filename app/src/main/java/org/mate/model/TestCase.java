6fa8b3ab45448bb4c720ca54818f29f8a7241a9b
Michael <auermich@fim.uni-passau.de>
2023-12-15 10:56:44 +0100

    fix: directly record stack trace upon occurrence (important since the crash distance function reads the stack trace after each action and the recording within the finish() method would be actually to late)

--- app/src/main/java/org/mate/model/TestCase.java
+++ app/src/main/java/org/mate/model/TestCase.java
@@ -157,14 +157,6 @@ public class TestCase {
             TestCaseStatistics.recordStats(this);
         }
 
-        // record a stack trace
-        if (hasCrashDetected() && Properties.RECORD_STACK_TRACE()) {
-            Registry.getEnvironmentManager().writeFile("stack_traces/" + id + ".txt",
-                    crashStackTrace.getRawStackTraceLines()
-                            .stream()
-                            .collect(Collectors.joining("\n")));
-        }
-
         // TODO: log the test case actions in a proper format
     }
 
@@ -455,6 +447,10 @@ public class TestCase {
                 setCrashDetected();
                 if (Properties.RECORD_STACK_TRACE()) {
                     crashStackTrace = Registry.getUiAbstractionLayer().getLastCrashStackTrace();
+                    Registry.getEnvironmentManager().writeFile("stack_traces/" + id + ".txt",
+                            crashStackTrace.getRawStackTraceLines()
+                                    .stream()
+                                    .collect(Collectors.joining("\n")));
                 }
             case SUCCESS_OUTBOUND:
                 return false;
