8b7d4ff8eaaa5bdce7555126f537de4075dcc7ee
Michael <auermich@fim.uni-passau.de>
2023-12-19 11:30:12 +0100

    overwrite logCurrentFitness() to use cached fitness values from fitness function wrappers

--- app/src/main/java/org/mate/exploration/genetic/algorithm/EDA.java
+++ app/src/main/java/org/mate/exploration/genetic/algorithm/EDA.java
@@ -1,13 +1,22 @@
 package org.mate.exploration.genetic.algorithm;
 
 import org.mate.MATE;
+import org.mate.Properties;
 import org.mate.Registry;
+import org.mate.exploration.genetic.chromosome.IChromosome;
 import org.mate.exploration.genetic.chromosome_factory.IChromosomeFactory;
 import org.mate.exploration.genetic.core.GeneticAlgorithm;
+import org.mate.exploration.genetic.fitness.ActionFitnessFunctionWrapper;
+import org.mate.exploration.genetic.fitness.GenotypePhenotypeMappedFitnessFunction;
+import org.mate.exploration.genetic.fitness.IActionFitnessFunction;
 import org.mate.exploration.genetic.fitness.IFitnessFunction;
 import org.mate.exploration.genetic.termination.ITerminationCondition;
 import org.mate.exploration.genetic.util.eda.IProbabilisticModel;
+import org.mate.model.TestCase;
+import org.mate.utils.coverage.Coverage;
+import org.mate.utils.coverage.CoverageUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,6 +30,12 @@ public class EDA<T> extends GeneticAlgorithm<T> {
      * The probabilistic model encoding information about the current population.
      */
     private final IProbabilisticModel<T> probabilisticModel;
+
+    /**
+     * Wraps the underlying fitness functions such that we can store and retrieve the fitness after
+     * individual actions.
+     */
+    private final List<ActionFitnessFunctionWrapper> fitnessFunctions = new ArrayList<>();
 
     /**
      * Initialises the estimation of distribution algorithm with the necessary attributes.
@@ -39,6 +54,10 @@ public class EDA<T> extends GeneticAlgorithm<T> {
                 fitnessFunctions, terminationCondition, populationSize,
                 populationSize, 0.0, 0.0);
         this.probabilisticModel = probabilisticModel;
+        for (IFitnessFunction<T> fitnessFunction : fitnessFunctions) {
+            this.fitnessFunctions.add(new ActionFitnessFunctionWrapper(
+                    (IActionFitnessFunction<TestCase>) fitnessFunction));
+        }
     }
 
     /**
@@ -77,5 +96,51 @@ public class EDA<T> extends GeneticAlgorithm<T> {
         logCurrentFitness();
         Registry.getEnvironmentManager().invalidateTracesCache();
         currentGenerationNumber++;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected <S> void logCurrentFitness() {
+
+        MATE.log_acc("Fitness of generation #" + (currentGenerationNumber + 1) + ":");
+
+        // TODO: Use chromosome id in logs instead of natural index + find a better solution for
+        //  multi-objective algorithms + a fix for MIO/MOSA/NSGA-II used in combination with GE.
+
+        for (int i = 0; i < this.fitnessFunctions.size(); i++) {
+            MATE.log_acc("Fitness function " + (i + 1) + ":");
+            // We can use the cached fitness values here and avoid an unnecessary re-computation.
+            final ActionFitnessFunctionWrapper fitnessFunction = this.fitnessFunctions.get(i);
+            for (int j = 0; j < population.size(); j++) {
+                IChromosome<TestCase> chromosome = (IChromosome<TestCase>) population.get(j);
+                MATE.log_acc("Chromosome " + (j + 1) + ": " + fitnessFunction.getFitness(chromosome));
+            }
+        }
+
+        if (Properties.COVERAGE() != Coverage.NO_COVERAGE) {
+
+            MATE.log_acc("Combined coverage until now: "
+                    + CoverageUtils.getCombinedCoverage(Properties.COVERAGE()));
+
+            if (Properties.GENO_TO_PHENO_TYPE_MAPPING()) {
+
+                final List<IChromosome<T>> phenotypePopulation = new ArrayList<>();
+
+                for (IChromosome<T> chromosome : population) {
+                    // TODO: Fix this mismatch between the type variables!
+                    phenotypePopulation.add(
+                            GenotypePhenotypeMappedFitnessFunction
+                                    .getPhenoType((IChromosome<S>) chromosome));
+                }
+
+                MATE.log_acc("Combined coverage of current population: "
+                        + CoverageUtils.getCombinedCoverage(Properties.COVERAGE(), phenotypePopulation));
+            } else {
+                MATE.log_acc("Combined coverage of current population: "
+                        + CoverageUtils.getCombinedCoverage(Properties.COVERAGE(), population));
+            }
+        }
     }
 }
